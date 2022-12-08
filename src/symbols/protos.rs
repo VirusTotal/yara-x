0417c5954b90f3298f0e23b3b2e4ebd136fb0330
Victor M. Alvarez <vmalvarez@virustotal.com>
2022-12-08 10:42:03 +0100

    Simplify implementation of SymbolLookup for protobufs.

--- src/symbols/protos.rs
+++ src/symbols/protos.rs
@@ -1,8 +1,8 @@
 use std::rc::Rc;
 
 use protobuf::reflect::{
-    EnumDescriptor, FieldDescriptor, MessageDescriptor, RuntimeFieldType,
-    RuntimeType,
+    EnumDescriptor, FieldDescriptor, MessageDescriptor, ReflectValueRef,
+    RuntimeFieldType, RuntimeType,
 };
 
 use protobuf::MessageDyn;
@@ -33,111 +33,10 @@ impl<'a> SymbolLookup<'a> for ProtoMessage {
         let message_descriptor = self.msg.descriptor_dyn();
         if let Some(field) = message_descriptor.field_by_name(ident) {
             match field.runtime_field_type() {
-                RuntimeFieldType::Singular(ty) => match ty {
-                    RuntimeType::I32 => {
-                        let value = field
-                            .get_singular(self.msg.as_ref())
-                            .and_then(|v| v.to_i32())
-                            .map(Value::from)
-                            .unwrap_or(Value::Unknown);
-                        Some(Symbol::new(
-                            Type::Integer,
-                            SymbolValue::Value(value),
-                        ))
-                    }
-                    RuntimeType::I64 => {
-                        let value = field
-                            .get_singular(self.msg.as_ref())
-                            .and_then(|v| v.to_i64())
-                            .map(Value::from)
-                            .unwrap_or(Value::Unknown);
-                        Some(Symbol::new(
-                            Type::Integer,
-                            SymbolValue::Value(value),
-                        ))
-                    }
-                    RuntimeType::U32 => {
-                        let value = field
-                            .get_singular(self.msg.as_ref())
-                            .and_then(|v| v.to_u32())
-                            .map(Value::from)
-                            .unwrap_or(Value::Unknown);
-                        Some(Symbol::new(
-                            Type::Integer,
-                            SymbolValue::Value(value),
-                        ))
-                    }
-                    RuntimeType::U64 => {
-                        todo!()
-                    }
-                    RuntimeType::F32 => {
-                        let value = field
-                            .get_singular(self.msg.as_ref())
-                            .and_then(|v| v.to_f32())
-                            .map(Value::from)
-                            .unwrap_or(Value::Unknown);
-                        Some(Symbol::new(
-                            Type::Float,
-                            SymbolValue::Value(value),
-                        ))
-                    }
-                    RuntimeType::F64 => {
-                        let value = field
-                            .get_singular(self.msg.as_ref())
-                            .and_then(|v| v.to_f64())
-                            .map(Value::from)
-                            .unwrap_or(Value::Unknown);
-                        Some(Symbol::new(
-                            Type::Float,
-                            SymbolValue::Value(value),
-                        ))
-                    }
-                    RuntimeType::Bool => {
-                        let value = field
-                            .get_singular(self.msg.as_ref())
-                            .and_then(|v| v.to_bool())
-                            .map(Value::from)
-                            .unwrap_or(Value::Unknown);
-                        Some(Symbol::new(
-                            Type::Bool,
-                            SymbolValue::Value(value),
-                        ))
-                    }
-                    RuntimeType::Enum(_) => {
-                        let value = field
-                            .get_singular(self.msg.as_ref())
-                            .and_then(|v| v.to_enum_value())
-                            .map(Value::from)
-                            .unwrap_or(Value::Unknown);
-                        Some(Symbol::new(
-                            Type::Integer,
-                            SymbolValue::Value(value),
-                        ))
-                    }
-                    RuntimeType::String | RuntimeType::VecU8 => {
-                        let value = if let Some(v) =
-                            field.get_singular(self.msg.as_ref())
-                        {
-                            v.to_str()
-                                .map(Value::from)
-                                .unwrap_or(Value::Unknown)
-                        } else {
-                            Value::Unknown
-                        };
-                        Some(Symbol::new(
-                            Type::String,
-                            SymbolValue::Value(value),
-                        ))
-                    }
-                    RuntimeType::Message(_) => {
-                        Some(Symbol::new_struct(Rc::new(ProtoMessage {
-                            msg: field
-                                .get_message(self.msg.as_ref())
-                                .clone_box()
-                                .into(),
-                        })))
-                    }
-                },
+                RuntimeFieldType::Singular(ty) => {
+                    let value = field.get_singular(self.msg.as_ref());
+                    to_symbol(ty, value)
+                }
                 RuntimeFieldType::Repeated(_) => {
                     Some(Symbol::new_array(Rc::new(ProtoRepeatedField {
                         msg: self.msg.clone(),
@@ -166,38 +65,88 @@ pub struct ProtoRepeatedField {
 
 impl<'a> SymbolIndex<'a> for ProtoRepeatedField {
     fn index(&self, index: usize) -> Option<Symbol<'a>> {
-        let item =
-            self.field_descriptor.get_repeated(self.msg.as_ref()).get(index);
-
-        match item.get_type() {
-            RuntimeType::I32 => {
-                let value =
-                    item.to_i32().map(Value::from).unwrap_or(Value::Unknown);
-                Some(Symbol::new(Type::Integer, SymbolValue::Value(value)))
-            }
-            RuntimeType::I64 => {
-                let value =
-                    item.to_i64().map(Value::from).unwrap_or(Value::Unknown);
-                Some(Symbol::new(Type::Integer, SymbolValue::Value(value)))
-            }
-            RuntimeType::U32 => todo!(),
-            RuntimeType::U64 => todo!(),
-            RuntimeType::F32 => todo!(),
-            RuntimeType::F64 => todo!(),
-            RuntimeType::Bool => todo!(),
-            RuntimeType::String => todo!(),
-            RuntimeType::VecU8 => todo!(),
-            RuntimeType::Enum(_) => todo!(),
-            RuntimeType::Message(_) => {
-                return Some(Symbol::new_struct(Rc::new(ProtoMessage {
-                    msg: item.to_message().unwrap().clone_box().into(),
-                })))
-            }
-        }
+        let msg = self.msg.as_ref();
+        let item = self.field_descriptor.get_repeated(msg).get(index);
+        to_symbol(item.get_type(), Some(item))
     }
 
     fn item_type(&self) -> Type {
         self.field_descriptor.item_type()
+    }
+}
+
+fn to_symbol<'a>(
+    ty: RuntimeType,
+    value: Option<ReflectValueRef>,
+) -> Option<Symbol<'a>> {
+    match ty {
+        RuntimeType::I32 => {
+            let value = value
+                .and_then(|v| v.to_i32())
+                .map(Value::from)
+                .unwrap_or(Value::Unknown);
+            Some(Symbol::new(Type::Integer, SymbolValue::Value(value)))
+        }
+        RuntimeType::I64 => {
+            let value = value
+                .and_then(|v| v.to_i64())
+                .map(Value::from)
+                .unwrap_or(Value::Unknown);
+            Some(Symbol::new(Type::Integer, SymbolValue::Value(value)))
+        }
+        RuntimeType::U32 => {
+            let value = value
+                .and_then(|v| v.to_u32())
+                .map(Value::from)
+                .unwrap_or(Value::Unknown);
+            Some(Symbol::new(Type::Integer, SymbolValue::Value(value)))
+        }
+        RuntimeType::U64 => todo!(),
+        RuntimeType::F32 => {
+            let value = value
+                .and_then(|v| v.to_f32())
+                .map(Value::from)
+                .unwrap_or(Value::Unknown);
+            Some(Symbol::new(Type::Float, SymbolValue::Value(value)))
+        }
+        RuntimeType::F64 => {
+            let value = value
+                .and_then(|v| v.to_f64())
+                .map(Value::from)
+                .unwrap_or(Value::Unknown);
+            Some(Symbol::new(Type::Float, SymbolValue::Value(value)))
+        }
+        RuntimeType::Bool => {
+            let value = value
+                .and_then(|v| v.to_bool())
+                .map(Value::from)
+                .unwrap_or(Value::Unknown);
+            Some(Symbol::new(Type::Float, SymbolValue::Value(value)))
+        }
+        RuntimeType::Enum(_) => {
+            let value = value
+                .and_then(|v| v.to_enum_value())
+                .map(Value::from)
+                .unwrap_or(Value::Unknown);
+            Some(Symbol::new(Type::String, SymbolValue::Value(value)))
+        }
+        RuntimeType::String | RuntimeType::VecU8 => {
+            let value = if let Some(value_ref) = value {
+                value_ref.to_str().map(Value::from).unwrap_or(Value::Unknown)
+            } else {
+                Value::Unknown
+            };
+            Some(Symbol::new(Type::String, SymbolValue::Value(value)))
+        }
+        RuntimeType::Message(message_descriptor) => {
+            if let Some(value_ref) = value {
+                Some(Symbol::new_struct(Rc::new(ProtoMessage {
+                    msg: value_ref.to_message().unwrap().clone_box().into(),
+                })))
+            } else {
+                Some(Symbol::new_struct(Rc::new(message_descriptor)))
+            }
+        }
     }
 }
 
@@ -221,10 +170,8 @@ impl<'a> SymbolLookup<'a> for MessageDescriptor {
 
         if let Some(field) = self.field_by_name(ident) {
             match field.runtime_field_type() {
-                RuntimeFieldType::Singular(ty) => {
-                    Some(runtime_type_to_symbol(ty))
-                }
-                RuntimeFieldType::Repeated(ty) => {
+                RuntimeFieldType::Singular(ty) => to_symbol(ty, None),
+                RuntimeFieldType::Repeated(_) => {
                     Some(Symbol::new_array(Rc::new(field)))
                 }
                 RuntimeFieldType::Map(_, _) => {
@@ -270,21 +217,5 @@ impl<'a> SymbolIndex<'a> for FieldDescriptor {
             },
             _ => unreachable!(),
         }
-    }
-}
-
-fn runtime_type_to_symbol<'a>(rt: RuntimeType) -> Symbol<'a> {
-    match rt {
-        RuntimeType::U64 => {
-            todo!()
-        }
-        RuntimeType::I32
-        | RuntimeType::I64
-        | RuntimeType::U32
-        | RuntimeType::Enum(_) => Type::Integer.into(),
-        RuntimeType::F32 | RuntimeType::F64 => Type::Float.into(),
-        RuntimeType::Bool => Type::Bool.into(),
-        RuntimeType::String | RuntimeType::VecU8 => Type::String.into(),
-        RuntimeType::Message(m) => Symbol::new_struct(Rc::new(m)),
     }
 }
