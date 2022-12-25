4e62109382443f0056fa7a4849ff7685103c94c9
Victor M. Alvarez <vmalvarez@virustotal.com>
2022-12-25 11:22:02 +0100

    Test enums with arbitrary values.

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -287,7 +287,10 @@ fn test_proto2_module() {
 
     condition_true!(r#"test_proto2.Enumeration.ITEM_0 == 0"#);
     condition_true!(r#"test_proto2.Enumeration.ITEM_1 == 1"#);
-    condition_true!(r#"test_proto2.TopLevelEnumeration.ITEM_1 == 1"#);
+
+    condition_true!(
+        r#"test_proto2.TopLevelEnumeration.ITEM_0x1000 == 0x1000"#
+    );
 
     condition_true!(r#"test_proto2.map_string_int64["one"] == 1"#);
 
