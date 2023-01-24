d8915ee56eabe612b3cf9586889a7bc7d7b0cf80
Victor M. Alvarez <vmalvarez@virustotal.com>
2023-01-24 19:43:45 +0100

    Add a few comments documenting `AddIndentationSpaces`

--- yara-x-fmt/src/indentation.rs
+++ yara-x-fmt/src/indentation.rs
@@ -2,6 +2,12 @@ use crate::{Token, TokenStream};
 use std::collections::VecDeque;
 
 /// Pipeline that insert spaces for indenting code.
+///
+/// This pipeline expects a token stream that contains indentation tokens
+/// (i.e: [`Token::Indentation`]) for increasing/decreasing the indentation
+/// level. These tokens are removed from the output, and the appropriate
+/// number of spaces is inserted after each newline for indenting the code
+/// to its corresponding level.
 pub(crate) struct AddIndentationSpaces<'a, T>
 where
     T: TokenStream<'a>,
@@ -35,9 +41,13 @@ where
         // No tokens in the output buffer, read tokens from input.
         for token in self.input.by_ref() {
             match token {
-                Token::Indentation(increase) => {
-                    self.indent_level += increase;
+                // Indentation tokens alter the current indentation level. The
+                // value for `delta` can be positive and negative.
+                Token::Indentation(delta) => {
+                    self.indent_level += delta;
                 }
+                // After each newline insert the appropriate number of spaces
+                // for indenting the line to the current level.
                 Token::Newline => {
                     self.output_buffer.push_back(Token::Newline);
                     for _ in 0..self.indent_level {
@@ -47,6 +57,7 @@ where
                     }
                     return self.output_buffer.pop_front();
                 }
+                // Any other token goes directly to the output.
                 _ => {
                     self.output_buffer.push_back(token);
                     return self.output_buffer.pop_front();
