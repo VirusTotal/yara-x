4f7fe16b392b073347b4b5c0f5ecd4c001f8e35b
Ronnie Salomonsen <ronnie.salomonsen@gmail.com>
2023-04-19 21:49:36 +0200

    Updated code to fix Clippy issues.

--- yara-x/src/modules/time.rs
+++ yara-x/src/modules/time.rs
@@ -5,16 +5,12 @@ use std::time::{SystemTime, UNIX_EPOCH};
 #[module_main]
 fn main(_ctx: &ScanContext) -> Time {
     // Nothing to do, but we have to return our protobuf
-    let time_proto = Time::new();
-    time_proto
+    Time::new()
 }
 
 #[module_export]
 fn now(ctx: &ScanContext) -> Option<i64> {
-    match SystemTime::now().duration_since(UNIX_EPOCH) {
-        Ok(n) => return Some(n.as_secs() as i64),
-        Err(_) => return None,
-    }
+    Some(SystemTime::now().duration_since(UNIX_EPOCH).ok()?.as_secs() as i64)
 }
 
 #[cfg(test)]
