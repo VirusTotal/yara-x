4ce15ca1b190ab7b30e9d61e30aa389dfed0f024
Victor M. Alvarez <vmalvarez@virustotal.com>
2023-05-24 10:57:27 +0200

    chore: remove unused test cases

--- yara-x-parser/src/parser/tests/ast.rs
+++ yara-x-parser/src/parser/tests/ast.rs
@@ -1,10 +1,8 @@
-use std::collections::hash_map::DefaultHasher;
 use std::fmt::Write;
 use std::fs;
-use std::hash::{Hash, Hasher};
 use std::path::PathBuf;
 
-use pretty_assertions::{assert_eq, assert_ne};
+use pretty_assertions::assert_eq;
 use yaml_rust::{Yaml, YamlLoader};
 
 use crate::parser::Parser;
@@ -70,69 +68,4 @@ fn ast() {
             }
         }
     }
-}
-
-fn hash<T: Hash>(obj: T) -> u64 {
-    let mut hasher = DefaultHasher::new();
-    obj.hash(&mut hasher);
-    hasher.finish()
-}
-
-#[test]
-fn pattern_hashes() {
-    let ast = Parser::new()
-        .build_ast(
-            r#"rule test { 
-            strings: 
-                $0 = "abc"
-                $1 = "abc"
-                $2 = "abc" nocase
-                $3 = "abcd"
-                $4 = "abc" wide
-                $5 = "abc" nocase wide
-                $6 = "abc" wide nocase
-                $7 = "abc" xor(1)
-                $8 = "abc" xor(1)
-                $9 = "abc" xor(2)
-                $10 = { 00 ?1 [0-1] 02 [1-] 03 [10] 04 [-] 05 ( 06 | 07 ) 08 ?? 09 }
-                $11 = { 00 ?1 [0-1] 02 [1-] 03 [10] 04 [-] 05 ( 06 | 07 ) 08 ?? 09 }
-                $12= { 00 01 02 }
-                $13 = { 0? 01 02 }
-            condition: 
-                all of them
-            }"#,
-        )
-        .unwrap();
-
-    let patterns = ast.rules[0].patterns.as_ref().unwrap();
-
-    // "abc" == "abc"
-    assert_eq!(hash(&patterns[0]), hash(&patterns[1]));
-
-    // "abc" != "abc" nocase
-    assert_ne!(hash(&patterns[0]), hash(&patterns[2]));
-
-    // "abc" != "abcd"
-    assert_ne!(hash(&patterns[0]), hash(&patterns[3]));
-
-    // "abc" nocase != "abc" wide
-    assert_ne!(hash(&patterns[2]), hash(&patterns[4]));
-
-    // "abc" wide != "abc" nocase wide
-    assert_ne!(hash(&patterns[4]), hash(&patterns[5]));
-
-    // "abc" nocase wide == "abc" wide nocase. The order is not relevant.
-    assert_eq!(hash(&patterns[5]), hash(&patterns[6]));
-
-    // "abc" xor(1) == "abc" xor(1)
-    assert_eq!(hash(&patterns[7]), hash(&patterns[8]));
-
-    // "abc" xor(1) != "abc" xor(2)
-    assert_ne!(hash(&patterns[8]), hash(&patterns[9]));
-
-    // { 00 ?1 [0-1] 02 [1-] 03 [10] 04 [-] 05 ( 06 | 07 ) 08 ?? 09 }
-    assert_eq!(hash(&patterns[10]), hash(&patterns[11]));
-
-    // { 0? 01 02 } != { 00 01 02 }
-    assert_ne!(hash(&patterns[12]), hash(&patterns[13]));
 }
