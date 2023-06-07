5670ff89b2a61b3a2251529d69c40f2d41c526a1
Victor M. Alvarez <vmalvarez@virustotal.com>
2023-06-07 15:43:57 +0200

    test(py): add more test cases for global variables

--- yara-x-py/tests/test_api.py
+++ yara-x-py/tests/test_api.py
@@ -11,17 +11,60 @@ def test_bad_variable_type():
   with pytest.raises(TypeError):
     compiler.define_global()
 
-def test_globals():
+def test_int_globals():
   compiler = yara_x.Compiler()
   compiler.define_global('some_int', 1);
   compiler.add_source('rule test {condition: some_int == 1}')
   rules = compiler.build()
+
   scanner = yara_x.Scanner(rules)
   matches = scanner.scan(b'')
   assert len(matches) == 1
+
   scanner.set_global('some_int', 2)
   matches = scanner.scan(b'')
   assert len(matches) == 0
+
+  scanner.set_global('some_int', 1)
+  matches = scanner.scan(b'')
+  assert len(matches) == 1
+
+def test_float_globals():
+  compiler = yara_x.Compiler()
+  compiler.define_global('some_float', 1.0);
+  compiler.add_source('rule test {condition: some_float == 1.0}')
+  rules = compiler.build()
+
+  scanner = yara_x.Scanner(rules)
+  matches = scanner.scan(b'')
+  assert len(matches) == 1
+
+  scanner.set_global('some_float', 2.0)
+  matches = scanner.scan(b'')
+  assert len(matches) == 0
+
+  scanner.set_global('some_float', 1.0)
+  matches = scanner.scan(b'')
+  assert len(matches) == 1
+
+def test_str_globals():
+  compiler = yara_x.Compiler()
+  compiler.define_global('some_str', 'foo');
+  compiler.add_source('rule test {condition: some_str == "foo"}')
+  rules = compiler.build()
+
+  scanner = yara_x.Scanner(rules)
+  matches = scanner.scan(b'')
+  assert len(matches) == 1
+
+  scanner.set_global('some_str', 'bar')
+  matches = scanner.scan(b'')
+  assert len(matches) == 0
+
+  scanner.set_global('some_str', 'foo')
+  matches = scanner.scan(b'')
+  assert len(matches) == 1
+
 
 def test_compile_and_scan():
   rules = yara_x.compile('rule foo {strings: $a = "foo" condition: $a}')
