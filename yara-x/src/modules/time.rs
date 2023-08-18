56605a5ffb924e905d5217c3963eba4e5a361376
Victor M. Alvarez <vmalvarez@virustotal.com>
2023-08-18 16:21:30 +0200

    fix: tests cases for `time` module.

--- yara-x/src/modules/time.rs
+++ yara-x/src/modules/time.rs
@@ -9,7 +9,7 @@ fn main(_ctx: &ScanContext) -> Time {
 }
 
 #[module_export]
-fn now(ctx: &ScanContext) -> Option<i64> {
+fn now(_ctx: &ScanContext) -> Option<i64> {
     Some(SystemTime::now().duration_since(UNIX_EPOCH).ok()?.as_secs() as i64)
 }
 
@@ -17,7 +17,9 @@ fn now(ctx: &ScanContext) -> Option<i64> {
 mod tests {
     #[test]
     fn end2end() {
-        let rules = crate::compiler::Compiler::new()
+        let mut compiler = crate::compiler::Compiler::new();
+
+        compiler
             .add_source(
                 r#"import "time"
                 rule rule_1 { condition: time.now() >= 0 }
@@ -26,12 +28,11 @@ mod tests {
                 rule rule_4 { condition: time.now() == 0 }
                 "#,
             )
-            .unwrap()
-            .build()
             .unwrap();
 
+        let rules = compiler.build();
         let mut scanner = crate::scanner::Scanner::new(&rules);
 
-        assert_eq!(scanner.scan(&[]).num_matching_rules(), 2);
+        assert_eq!(scanner.scan(&[]).unwrap().matching_rules().len(), 2);
     }
 }
