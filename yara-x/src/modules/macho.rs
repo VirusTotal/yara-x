81086d5a61495eb33096aa3e528c643118d7a574
Tomáš Ďuriš <duristomas67@gmail.com>
2023-08-25 15:20:51 +0200

    Add header files parsed, support 32/64 and BE/LE files

--- yara-x/src/modules/macho.rs
+++ yara-x/src/modules/macho.rs
@@ -1,8 +1,21 @@
 use crate::modules::prelude::*;
 use crate::modules::protos::macho::*;
 
+use byteorder::{BigEndian, ByteOrder};
 use std::convert::TryInto;
 
+// Define Mach-O header constats
+// May be moved away later
+const MH_MAGIC: u32 = 0xFEEDFACE;
+const MH_CIGAM: u32 = 0xCEFAEDFE;
+const MH_MAGIC_64: u32 = 0xFEEDFACF;
+const MH_CIGAM_64: u32 = 0xCFFAEDFE;
+const FAT_MAGIC: u32 = 0xCAFEBABE;
+const FAT_CIGAM: u32 = 0xBEBAFECA;
+const FAT_MAGIC_64: u32 = 0xCAFEBABF;
+const FAT_CIGAM_64: u32 = 0xBFBAFECA;
+
+// 32bit Mach-O header struct
 #[repr(C)]
 #[derive(Debug, Default, Clone, Copy)]
 struct MachOHeader32 {
@@ -15,6 +28,7 @@ struct MachOHeader32 {
     flags: u32,
 }
 
+// 64bit Mach-O header struct
 #[repr(C)]
 #[derive(Debug, Default, Clone, Copy)]
 struct MachOHeader64 {
@@ -28,16 +42,67 @@ struct MachOHeader64 {
     reserved: u32,
 }
 
+// Check if given file is basic Mach-O file
+fn is_macho_file_block(data: &[u8]) -> bool {
+    let magic = u32::from_ne_bytes(data[0..4].try_into().unwrap());
+    match magic {
+        MH_MAGIC | MH_CIGAM | MH_MAGIC_64 | MH_CIGAM_64 => true,
+        _ => false,
+    }
+}
+
+// Check if given file is FAT Mach-O file
+fn is_fat_macho_file_block(data: &[u8]) -> bool {
+    let magic = u32::from_ne_bytes(data[0..4].try_into().unwrap());
+    match magic {
+        FAT_MAGIC | FAT_CIGAM | FAT_MAGIC_64 | FAT_CIGAM_64 => true,
+        _ => false,
+    }
+}
+
+// Check if given file is 32bit Mach-O file
 fn is_32_bit(magic: u32) -> bool {
     let bytes = magic.to_ne_bytes();
     bytes[0] == 0xce || bytes[3] == 0xce
 }
 
-fn parse_macho_header(data: &[u8]) {
+// If given file is BigEndian we want to swap bytes to LittleEndian
+// If file is already in LittleEndian format return false
+fn should_swap_bytes(magic: u32) -> bool {
+    match magic {
+        MH_CIGAM | MH_CIGAM_64 | FAT_CIGAM | FAT_CIGAM_64 => true,
+        _ => false,
+    }
+}
+
+// Swap Mach-O headers from BigEndian to LittleEndian
+fn swap_mach_header(header: &mut MachOHeader64) {
+    if should_swap_bytes(header.magic) {
+        header.cputype = BigEndian::read_u32(&header.cputype.to_le_bytes());
+        header.cpusubtype =
+            BigEndian::read_u32(&header.cpusubtype.to_le_bytes());
+        header.filetype = BigEndian::read_u32(&header.filetype.to_le_bytes());
+        header.ncmds = BigEndian::read_u32(&header.ncmds.to_le_bytes());
+        header.sizeofcmds =
+            BigEndian::read_u32(&header.sizeofcmds.to_le_bytes());
+        header.flags = BigEndian::read_u32(&header.flags.to_le_bytes());
+
+        // Only swap the reserved field for 64-bit files
+        if !is_32_bit(header.magic) {
+            header.reserved =
+                BigEndian::read_u32(&header.reserved.to_le_bytes());
+        }
+    }
+}
+
+// Parse basic Mach-O file
+fn parse_macho_file(data: &[u8], macho_proto: &mut Macho) {
+    // File is too small to contain Mach-O header
     if data.len() < std::mem::size_of::<MachOHeader64>() {
         return;
     }
 
+    // Get header size by determining if file is 32 or 64 bit
     let header_size =
         if is_32_bit(u32::from_ne_bytes(data[0..4].try_into().unwrap())) {
             std::mem::size_of::<MachOHeader32>()
@@ -45,7 +110,8 @@ fn parse_macho_header(data: &[u8]) {
             std::mem::size_of::<MachOHeader64>()
         };
 
-    let header: MachOHeader64 = MachOHeader64 {
+    // Populate Mach-O header struct
+    let mut header: MachOHeader64 = MachOHeader64 {
         magic: u32::from_ne_bytes(data[0..4].try_into().unwrap()),
         cputype: u32::from_ne_bytes(data[4..8].try_into().unwrap()),
         cpusubtype: u32::from_ne_bytes(data[8..12].try_into().unwrap()),
@@ -59,6 +125,25 @@ fn parse_macho_header(data: &[u8]) {
             u32::from_ne_bytes(data[28..32].try_into().unwrap())
         },
     };
+
+    if should_swap_bytes(header.magic) {
+        swap_mach_header(&mut header);
+    }
+
+    // Set protobuf values for Mach-O header
+    macho_proto.set_magic(header.magic);
+    macho_proto.set_cputype(header.cputype);
+    macho_proto.set_cpusubtype(header.cpusubtype);
+    macho_proto.set_filetype(header.filetype);
+    macho_proto.set_ncmds(header.ncmds);
+    macho_proto.set_sizeofcmds(header.sizeofcmds);
+    macho_proto.set_flags(header.flags);
+
+    // Only set the reserved field in the protobuf for 64-bit files
+    if !is_32_bit(header.magic) {
+        macho_proto.set_reserved(header.reserved);
+    }
+
     // Print header fields in hexadecimal format
     println!("Magic: 0x{:x}", header.magic);
     println!("CPU Type: 0x{:x}", header.cputype);
@@ -73,13 +158,24 @@ fn parse_macho_header(data: &[u8]) {
 #[module_main]
 fn main(ctx: &ScanContext) -> Macho {
     // Create an empty instance of the Macho protobuf
-    let macho_proto = Macho::new();
+    let mut macho_proto = Macho::new();
 
     // Get a &[u8] slice with the content of the file being scanned.
     let data = ctx.scanned_data();
 
-    // Parse the MachO header
-    parse_macho_header(data);
+    // If data is too short to be valid Mach-O file, return empty protobuf
+    if data.len() < 4 {
+        println!("Data is too short to be a valid Mach-O file.");
+        return macho_proto;
+    }
+
+    if is_macho_file_block(data) {
+        parse_macho_file(data, &mut macho_proto);
+    }
+
+    if is_fat_macho_file_block(data) {
+        //parse_fat_macho_file(data, &mut macho_proto);
+    }
 
     macho_proto
 }
