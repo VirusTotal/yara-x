861e8fcfc01e4fd5cbb38806870a9309c46c46f9
Victor M. Alvarez <vmalvarez@virustotal.com>
2023-09-26 13:00:58 +0200

    feat: implement the `hash` module.
    The `hash` module implements hashing functions `md5`, `sha1` and `sha256`.

--- yara-x/src/modules/modules.rs
+++ yara-x/src/modules/modules.rs
@@ -1,6 +1,8 @@
 // File generated automatically by build.rs. Do not edit.
 #[cfg(feature = "text-module")]
 pub mod text;
+#[cfg(feature = "hash-module")]
+pub mod hash;
 #[cfg(feature = "test_proto2-module")]
 pub mod test_proto2;
 #[cfg(feature = "time-module")]
--- yara-x/src/tests/mod.rs
+++ yara-x/src/tests/mod.rs
@@ -2802,6 +2802,52 @@ fn test_defined_1() {
 }
 
 #[test]
+#[cfg(feature = "hash-module")]
+fn test_hash_module() {
+    rule_true!(
+        r#"
+        import "hash"
+        rule test {
+          condition:
+            hash.md5(0, filesize) == "6df23dc03f9b54cc38a0fc1483df6e21" and
+            hash.md5(3, 3) == "37b51d194a7513e45b56f6524f2d51f2" and
+            hash.md5(0, filesize) == hash.md5("foobarbaz") and
+            hash.md5(3, 3) ==  hash.md5("bar")
+        }
+        "#,
+        b"foobarbaz"
+    );
+
+    rule_true!(
+        r#"
+        import "hash"
+        rule test {
+          condition:
+            hash.sha1(0, filesize) == "5f5513f8822fdbe5145af33b64d8d970dcf95c6e" and
+            hash.sha1(3, 3) == "62cdb7020ff920e5aa642c3d4066950dd1f01f4d" and
+            hash.sha1(0, filesize) == hash.sha1("foobarbaz") and
+            hash.sha1(3, 3) ==  hash.sha1("bar")
+        }
+        "#,
+        b"foobarbaz"
+    );
+
+    rule_true!(
+        r#"
+        import "hash"
+        rule test {
+          condition:
+            hash.sha256(0, filesize) == "97df3588b5a3f24babc3851b372f0ba71a9dcdded43b14b9d06961bfc1707d9d" and
+            hash.sha256(3, 3) == "fcde2b2edba56bf408601fb721fe9b5c338d10ee429ea04fae5511b68fbf8fb9" and
+            hash.sha256(0, filesize) == hash.sha256("foobarbaz") and
+            hash.sha256(3, 3) ==  hash.sha256("bar")
+        }
+        "#,
+        b"foobarbaz"
+    );
+}
+
+#[test]
 #[cfg(feature = "test_proto2-module")]
 fn test_defined_2() {
     condition_false!(r#"defined test_proto2.undef_i64()"#);
