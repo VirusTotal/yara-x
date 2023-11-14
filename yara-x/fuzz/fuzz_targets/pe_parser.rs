26e03a04af86abd3698fcaea564b9848529ab31c
Victor M. Alvarez <plusvic@gmail.com>
2023-11-14 18:34:01 +0100

    chore: use public API in `pe` fuzzer

--- yara-x/fuzz/fuzz_targets/pe_parser.rs
+++ yara-x/fuzz/fuzz_targets/pe_parser.rs
@@ -1,7 +1,6 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use yara_x::modules::pe::parser::PEParser;
 
 fuzz_target!(|data: &[u8]| {
-    let _ = PEParser::new().parse(data);
+    let _ = yara_x::mods::invoke_mod::<yara_x::mods::PE>(data);
 });
