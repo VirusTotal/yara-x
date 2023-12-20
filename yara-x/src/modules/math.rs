cc0ad7e694f8178ffa7dcac5bd4fbc366e4d3331
Victor M. Alvarez <vmalvarez@virustotal.com>
2023-12-20 18:41:04 +0100

    feat: implement `math.serial_correlation`

--- yara-x/src/modules/math.rs
+++ yara-x/src/modules/math.rs
@@ -1,5 +1,7 @@
 use crate::modules::prelude::*;
 use crate::modules::protos::math::*;
+use itertools::Itertools;
+use num_traits::Pow;
 
 #[module_main]
 fn main(_data: &[u8]) -> Math {
@@ -62,6 +64,25 @@ fn mean_string(ctx: &ScanContext, s: RuntimeString) -> Option<f64> {
     mean(s.as_bstr(ctx).as_bytes())
 }
 
+#[module_export(name = "serial_correlation")]
+fn serial_correlation_data(
+    ctx: &ScanContext,
+    offset: i64,
+    length: i64,
+) -> Option<f64> {
+    let start = offset as usize;
+    let end = start.saturating_add(length as usize);
+    serial_correlation(ctx.scanned_data().get(start..end)?)
+}
+
+#[module_export(name = "serial_correlation")]
+fn serial_correlation_string(
+    ctx: &ScanContext,
+    s: RuntimeString,
+) -> Option<f64> {
+    serial_correlation(s.as_bstr(ctx).as_bytes())
+}
+
 fn entropy(data: &[u8]) -> Option<f64> {
     if data.is_empty() {
         return None;
@@ -117,6 +138,31 @@ fn mean(data: &[u8]) -> Option<f64> {
     }
 
     Some(sum / data.len() as f64)
+}
+
+fn serial_correlation(data: &[u8]) -> Option<f64> {
+    let mut scc1: f64 = data
+        .iter()
+        .map(|x| *x as f64)
+        .tuple_windows()
+        .map(|(x, y)| x * y)
+        .sum();
+
+    let scc2: f64 = data.iter().map(|x| *x as f64).sum::<f64>().pow(2);
+    let scc3: f64 = data.iter().map(|x| (*x as f64).pow(2)).sum();
+
+    if let (Some(first), Some(last)) = (data.first(), data.last()) {
+        scc1 += (*first as f64) * (*last as f64)
+    }
+
+    let len = data.len() as f64;
+    let scc = (len * scc1 - scc2) / (len * scc3 - scc2);
+
+    if scc.is_nan() {
+        Some(-100000.0)
+    } else {
+        Some(scc)
+    }
 }
 
 #[cfg(test)]
@@ -242,5 +288,38 @@ mod tests {
             }"#,
             b"ABCABC"
         );
+    }
+
+    #[test]
+    fn serial_correlation() {
+        rule_true!(
+            r#"
+            import "math"
+            rule test {
+                condition:
+                    math.serial_correlation("BCA") == -0.5
+            }"#,
+            &[]
+        );
+
+        rule_true!(
+            r#"
+            import "math"
+            rule test {
+                condition:
+                    math.serial_correlation(1, 3) == -0.5
+            }"#,
+            b"ABCABC"
+        );
+
+        rule_true!(
+            r#"
+            import "math"
+            rule test {
+                condition:
+                    math.serial_correlation(0, 0) == -100000.0
+            }"#,
+            b"ABCABC"
+        )
     }
 }
