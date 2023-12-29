ec34f7ed6e21a07224ae7d35454e72ef6cce2326
Victor M. Alvarez <vmalvarez@virustotal.com>
2023-12-29 11:08:10 +0100

    tests: fix test case

--- yara-x/src/wasm/builder.rs
+++ yara-x/src/wasm/builder.rs
@@ -470,38 +470,38 @@ mod tests {
         assert_eq!(
             text,
             r#"(module
-  (func (;147;) (type 1) (result i32)
+  (func (;149;) (type 1) (result i32)
     i32.const 0
     global.set 2
     i32.const 0
     global.set 3
-    call 148
-    call 149
+    call 150
+    call 151
     global.get 3
   )
-  (func (;148;) (type 0)
-    block ;; label = @1
-      call 150
-    end
-    block ;; label = @1
-      call 151
-    end
-  )
-  (func (;149;) (type 0)
+  (func (;150;) (type 0)
     block ;; label = @1
       call 152
     end
-  )
-  (func (;150;) (type 0)
-    i32.const 4
+    block ;; label = @1
+      call 153
+    end
   )
   (func (;151;) (type 0)
-    i32.const 5
+    block ;; label = @1
+      call 154
+    end
   )
   (func (;152;) (type 0)
+    i32.const 4
+  )
+  (func (;153;) (type 0)
+    i32.const 5
+  )
+  (func (;154;) (type 0)
     i32.const 6
   )
-  (export "main" (func 147))
+  (export "main" (func 149))
 )"#
         );
     }
