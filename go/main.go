18b752a2447fa8775ad7de0a7e01698583581ac6
Victor M. Alvarez <vmalvarez@virustotal.com>
2024-02-09 16:25:41 +0100

    ci: set `-rpath`

--- go/main.go
+++ go/main.go
@@ -6,7 +6,7 @@
 package yara_x
 
 // #cgo CFLAGS: -I${SRCDIR}/../capi/include
-// #cgo !static_link LDFLAGS: -L${SRCDIR}/../target/release -lyara_x_c
+// #cgo !static_link LDFLAGS: -L${SRCDIR}/../target/release -Wl,-rpath,'${SRCDIR}/../target/release' -lyara_x_c
 // #cgo static_link LDFLAGS: ${SRCDIR}/../target/release/libyara_x_c.a
 // #include <yara-x.h>
 import "C"
