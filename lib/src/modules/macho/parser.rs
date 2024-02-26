154994e9e1cb1c24a8e08313dca9740f97a8fa27
Victor M. Alvarez <vmalvarez@virustotal.com>
2024-02-26 08:55:34 +0100

    fix: remove the logic that parses signatures from Macho files.
    This code depended on the `cryptographic-message-syntax` crate, which introduces a ridiculous amount of indirect dependencies, including crates like `tokio` and `reqwest`. I don't know why a crate devoted to parsing cryptographic data structures depends on a HTTP client library.
    
    I've tried replacing `cryptographic-message-syntax` with `cms`, but it doesn't seems to be a straightforward change. We need to find a way to implement this feature using `cms` or some other crate that doesn't introduce insane dependencies.

--- lib/src/modules/macho/parser.rs
+++ lib/src/modules/macho/parser.rs
@@ -1,6 +1,5 @@
 use crate::modules::protos;
 use bstr::{BStr, ByteSlice};
-use cryptographic_message_syntax::SignedData;
 #[cfg(feature = "logging")]
 use log::error;
 use nom::bytes::complete::take;
@@ -33,7 +32,7 @@ const _CS_MAGIC_REQUIREMENTS: u32 = 0xfade0c01;
 const _CS_MAGIC_CODEDIRECTORY: u32 = 0xfade0c02;
 const _CS_MAGIC_EMBEDDED_SIGNATURE: u32 = 0xfade0cc0;
 const _CS_MAGIC_DETACHED_SIGNATURE: u32 = 0xfade0cc1;
-const CS_MAGIC_BLOBWRAPPER: u32 = 0xfade0b01;
+const _CS_MAGIC_BLOBWRAPPER: u32 = 0xfade0b01;
 const CS_MAGIC_EMBEDDED_ENTITLEMENTS: u32 = 0xfade7171;
 
 /// Mach-O dynamic linker constant
@@ -124,7 +123,7 @@ impl<'a> MachO<'a> {
                 FAT_MAGIC | FAT_CIGAM | FAT_MAGIC_64 | FAT_CIGAM_64
             )
         })
-            .parse(data)?;
+        .parse(data)?;
 
         // The magic number indicates the endianness.
         let endianness = match magic {
@@ -203,7 +202,7 @@ impl<'a> MachO<'a> {
         let (remainder, magic) = verify(be_u32, |magic| {
             matches!(*magic, MH_MAGIC | MH_CIGAM | MH_MAGIC_64 | MH_CIGAM_64)
         })
-            .parse(data)?;
+        .parse(data)?;
 
         let endianness = match magic {
             MH_MAGIC | MH_MAGIC_64 => Endianness::Big,
@@ -228,14 +227,14 @@ impl<'a> MachO<'a> {
                 cond(!is_32_bits, u32(endianness)), // reserved, only in 64-bits
             )),
             |(
-                 cputype,
-                 cpusubtype,
-                 filetype,
-                 ncmds,
-                 sizeofcmds,
-                 flags,
-                 reserved,
-             )| {
+                cputype,
+                cpusubtype,
+                filetype,
+                ncmds,
+                sizeofcmds,
+                flags,
+                reserved,
+            )| {
                 MachOHeader {
                     magic,
                     cputype,
@@ -395,19 +394,19 @@ impl<'a> MachOFile<'a> {
                 cond(!self.is_32_bits, u32(self.endianness)), // reserved3
             )),
             |(
-                 sectname,
-                 segname,
-                 addr,
-                 size,
-                 offset,
-                 align,
-                 reloff,
-                 nreloc,
-                 flags,
-                 reserved1,
-                 reserved2,
-                 reserved3,
-             )| {
+                sectname,
+                segname,
+                addr,
+                size,
+                offset,
+                align,
+                reloff,
+                nreloc,
+                flags,
+                reserved1,
+                reserved2,
+                reserved3,
+            )| {
                 Section {
                     sectname,
                     segname,
@@ -686,23 +685,23 @@ impl<'a> MachOFile<'a> {
                 u32(self.endianness), //  nlocrel
             )),
             |(
-                 ilocalsym,
-                 nlocalsym,
-                 iextdefsym,
-                 nextdefsym,
-                 tocoff,
-                 ntoc,
-                 modtaboff,
-                 nmodtab,
-                 extrefsymoff,
-                 nextrefsyms,
-                 indirectsymoff,
-                 nindirectsyms,
-                 extreloff,
-                 nextrel,
-                 locreloff,
-                 nlocrel,
-             )| {
+                ilocalsym,
+                nlocalsym,
+                iextdefsym,
+                nextdefsym,
+                tocoff,
+                ntoc,
+                modtaboff,
+                nmodtab,
+                extrefsymoff,
+                nextrefsyms,
+                indirectsymoff,
+                nindirectsyms,
+                extreloff,
+                nextrel,
+                locreloff,
+                nlocrel,
+            )| {
                 Dysymtab {
                     ilocalsym,
                     nlocalsym,
@@ -825,38 +824,6 @@ impl<'a> MachOFile<'a> {
                                 }
                             }
                         }
-                        CS_MAGIC_BLOBWRAPPER => {
-                            if let Ok(signage) = SignedData::parse_ber(
-                                &super_data
-                                    [offset + size_of_blob..offset + length],
-                            ) {
-                                let signers = signage.signers();
-                                let certs = signage.certificates();
-                                let mut cert_info = Certificates {
-                                    common_names: Vec::new(),
-                                    signer_names: Vec::new(),
-                                };
-
-                                certs.for_each(|cert| {
-                                    let name =
-                                        cert.subject_common_name().unwrap();
-                                    cert_info.common_names.push(name);
-                                });
-
-                                signers.for_each(|signer| {
-                                    let (name, _) = signer
-                                        .certificate_issuer_and_serial()
-                                        .unwrap();
-                                    cert_info.signer_names.push(
-                                        name.user_friendly_str()
-                                            .unwrap()
-                                            .to_string(),
-                                    );
-                                });
-
-                                self.certificates = Some(cert_info);
-                            }
-                        }
                         _ => {}
                     }
                 }
@@ -884,17 +851,17 @@ impl<'a> MachOFile<'a> {
                 u32(self.endianness), //  export_size
             )),
             |(
-                 rebase_off,
-                 rebase_size,
-                 bind_off,
-                 bind_size,
-                 weak_bind_off,
-                 weak_bind_size,
-                 lazy_bind_off,
-                 lazy_bind_size,
-                 export_off,
-                 export_size,
-             )| {
+                rebase_off,
+                rebase_size,
+                bind_off,
+                bind_size,
+                weak_bind_off,
+                weak_bind_size,
+                lazy_bind_off,
+                lazy_bind_size,
+                export_off,
+                export_size,
+            )| {
                 DyldInfo {
                     rebase_off,
                     rebase_size,
@@ -1679,7 +1646,7 @@ impl From<&MinVersion> for protos::macho::MinVersion {
             protobuf::EnumOrUnknown::<protos::macho::DEVICE_TYPE>::from_i32(
                 mv.device as i32,
             )
-                .unwrap(),
+            .unwrap(),
         );
         result.set_version(convert_to_version_string(mv.version));
         result.set_sdk(convert_to_version_string(mv.sdk));
