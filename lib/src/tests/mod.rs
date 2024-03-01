VirusTotal/yara-x

commit dc2c9ea99753e7fbb00a4cf38a368aba0cc4d4bd

fix: issue with wide regexps causing false positives
Strings matching wide regexps must contain interleaved zeroes, but we were not making sure that these bytes were actually zero when matching the regexp with `PikeVM`.

lib/src/re/thompson/pikevm.rs
@@ -1,3 +1,4 @@
+use std::cell::Cell;
 use std::mem;
 
 use bitvec::array::BitArray;
@@ -90,23 +91,41 @@ impl<'r> PikeVM<'r> {
                 self.try_match_impl(start, right.iter(), left.iter().rev(), f)
             }
             // Going forward, wide.
-            (false, true) => self.try_match_impl(
-                start,
-                right.iter().step_by(2),
-                left.iter().rev().skip(1).step_by(2),
-                |match_len| f(match_len * 2),
-            ),
+            (false, true) => {
+                let invalid_wide = Cell::new(false);
+                self.try_match_impl(
+                    start,
+                    WideIter::non_zero_first(right.iter(), &invalid_wide),
+                    WideIter::zero_first(left.iter().rev(), &invalid_wide),
+                    |match_len| {
+                        if invalid_wide.get() {
+                            Action::Stop
+                        } else {
+                            f(match_len * 2)
+                        }
+                    },
+                )
+            }
             // Going backward, not wide.
             (true, false) => {
                 self.try_match_impl(start, left.iter().rev(), right.iter(), f)
             }
             // Going backward, wide.
-            (true, true) => self.try_match_impl(
-                start,
-                left.iter().rev().skip(1).step_by(2),
-                right.iter().step_by(2),
-                |match_len| f(match_len * 2),
-            ),
+            (true, true) => {
+                let invalid_wide = Cell::new(false);
+                self.try_match_impl(
+                    start,
+                    WideIter::zero_first(left.iter().rev(), &invalid_wide),
+                    WideIter::non_zero_first(right.iter(), &invalid_wide),
+                    |match_len| {
+                        if invalid_wide.get() {
+                            Action::Stop
+                        } else {
+                            f(match_len * 2)
+                        }
+                    },
+                )
+            }
         }
     }
 
@@ -382,6 +401,97 @@ pub(crate) fn epsilon_closure<C: CodeLoc>(
                     state.threads.push(next)
                 }
             }
+        }
+    }
+}
+
+/// WideIter is an iterator that takes a byte iterator and consumes it two
+/// bytes at a time, returning one of the bytes and making sure that other
+/// is zero. Which of the two bytes is returned and which is zero depends
+/// on the kind of iterator you create. With [`WideIter::non_zero_first`]
+/// you create an iterator that expects the first byte of each pair to be
+/// non-zero, and the second one to be zero.
+///
+/// In the other hand, [`WideIter::zero_first`] expect the first byte of
+/// each pair to be zero, and the second one to be the non-zero byte
+/// returned by the iterator.   
+///
+/// ```ignore
+/// let error = Cell::new(false);
+/// let v = vec![1,0,2,0,3,0];
+/// // The non-zero values are expected to be the first of each pair.
+/// let i = WideIter::non_zero_first(v.iter(), &error);
+/// assert_eq!(i.collect(), vec![1,2,3]);
+/// // No error.
+/// assert!(!error_flag.get());
+/// ```
+///
+/// ```ignore
+/// let error = Cell::new(false);
+/// let v = vec![1,100,2,0,3,0];
+/// let i = WideIter::non_zero_first(v.iter(), &error);
+/// assert_eq!(i.collect(), vec![1,2,3]);
+/// // Error! between 1 and 2 there's a non-zero value 100
+/// assert!(error_flag.get());
+/// ```
+///
+/// ```ignore
+/// let error = Cell::new(false);
+/// let v = vec![0,1,0,2,0,3];
+/// // The zero values are expected to be the first of each pair.
+/// let i = WideIter::zero_first(v.iter(), &error);
+/// assert_eq!(i.collect(), vec![1,2,3]);
+/// // No error
+/// assert!(!error_flag.get());
+/// ```
+struct WideIter<'a, I>
+where
+    I: Iterator<Item = &'a u8>,
+{
+    iter: I,
+    error_flag: &'a Cell<bool>,
+    zero_first: bool,
+}
+
+impl<'a, I> WideIter<'a, I>
+where
+    I: Iterator<Item = &'a u8>,
+{
+    pub fn non_zero_first(iter: I, error_flag: &'a Cell<bool>) -> Self
+    where
+        I: Iterator<Item = &'a u8>,
+    {
+        WideIter { iter, error_flag, zero_first: false }
+    }
+
+    pub fn zero_first(iter: I, error_flag: &'a Cell<bool>) -> Self
+    where
+        I: Iterator<Item = &'a u8>,
+    {
+        WideIter { iter, error_flag, zero_first: true }
+    }
+}
+
+impl<'a, I> Iterator for WideIter<'a, I>
+where
+    I: Iterator<Item = &'a u8>,
+{
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let first_byte = self.iter.next()?;
+        let second_byte = self.iter.next()?;
+
+        if self.zero_first {
+            if *first_byte != 0_u8 {
+                self.error_flag.replace(true);
+            }
+            Some(second_byte)
+        } else {
+            if *second_byte != 0_u8 {
+                self.error_flag.replace(true);
+            }
+            Some(first_byte)
         }
     }
 }

lib/src/tests/mod.rs
@@ -1480,6 +1480,40 @@ fn regexp_wide() {
         b"f\x00o\x00o\x00b\x00a\x00r\x00b\x00a\x00z\x00",
         b"f\x00o\x00o\x00b\x00a\x00r\x00b\x00a\x00z\x00"
     );
+
+    pattern_false!(
+        r#"/foobar.[A-Z]{1}/ wide"#,
+        b"f\x00o\x00o\x00b\x00a\x00r\x00\x00\x15W\x00"
+    );
+
+    pattern_false!(
+        r#"/.[A-Z]{1}foobar/ wide"#,
+        b"\x00\x15W\x00f\x00o\x00o\x00b\x00a\x00r\x00"
+    );
+
+    pattern_false!(
+        r#"/\bfoobar/ wide"#,
+        b"x\x00f\x00o\x00o\x00b\x00a\x00r\x00"
+    );
+
+    pattern_true!(
+        r#"/\Bfoobar/ wide"#,
+        b"x\x00f\x00o\x00o\x00b\x00a\x00r\x00"
+    );
+
+    pattern_false!(
+        r#"/foobar\b/ wide"#,
+        b"f\x00o\x00o\x00b\x00a\x00r\x00x\x00"
+    );
+
+    pattern_true!(
+        r#"/foobar\B/ wide"#,
+        b"f\x00o\x00o\x00b\x00a\x00r\x00x\x00"
+    );
+
+    pattern_true!(r#"/foobar\b/ wide"#, b"f\x00o\x00o\x00b\x00a\x00r\x00x");
+    pattern_false!(r#"/foobar\B/ wide"#, b"f\x00o\x00o\x00b\x00a\x00r\x00x");
+    pattern_true!(r#"/foobar$/ wide"#, b"f\x00o\x00o\x00b\x00a\x00r\x00x");
 }
 
 #[test]

lib/src/re/fast/fastvm.rs
@@ -51,7 +51,7 @@ impl<'r> FastVM<'r> {
     /// VM before aborting.
     ///
     /// This sets a limit on the number of bytes that the VM will read from the
-    /// input while trying find a match. Without a limit, the VM will can incur
+    /// input while trying to find a match. Without a limit, the VM can incur
     /// in excessive execution time for regular expressions that are unbounded,
     /// like `foo.*bar`. For inputs that starts with `foo`, this regexp will
     /// try to scan the whole input, and that would take a long time if the

commit cd87d883788b4fd0928fc857f7e788af8d9da66c

fix: issue introduced in dc2c9ea99753e7fbb00a4cf38a368aba0cc4d4bd
The previous commit fixed part of the issue with wide regexps, but didn't fix it correctly.

lib/src/re/thompson/pikevm.rs
@@ -92,17 +92,15 @@ impl<'r> PikeVM<'r> {
             }
             // Going forward, wide.
             (false, true) => {
-                let invalid_wide = Cell::new(false);
+                let error_fwd = Cell::new(None);
+                let error_bck = Cell::new(None);
                 self.try_match_impl(
                     start,
-                    WideIter::non_zero_first(right.iter(), &invalid_wide),
-                    WideIter::zero_first(left.iter().rev(), &invalid_wide),
-                    |match_len| {
-                        if invalid_wide.get() {
-                            Action::Stop
-                        } else {
-                            f(match_len * 2)
-                        }
+                    WideIter::non_zero_first(right.iter(), &error_fwd),
+                    WideIter::zero_first(left.iter().rev(), &error_bck),
+                    |match_len| match error_fwd.get() {
+                        Some(pos) if pos < match_len => Action::Stop,
+                        _ => f(match_len * 2),
                     },
                 )
             }
@@ -112,17 +110,15 @@ impl<'r> PikeVM<'r> {
             }
             // Going backward, wide.
             (true, true) => {
-                let invalid_wide = Cell::new(false);
+                let error_fwd = Cell::new(None);
+                let error_bck = Cell::new(None);
                 self.try_match_impl(
                     start,
-                    WideIter::zero_first(left.iter().rev(), &invalid_wide),
-                    WideIter::non_zero_first(right.iter(), &invalid_wide),
-                    |match_len| {
-                        if invalid_wide.get() {
-                            Action::Stop
-                        } else {
-                            f(match_len * 2)
-                        }
+                    WideIter::zero_first(left.iter().rev(), &error_fwd),
+                    WideIter::non_zero_first(right.iter(), &error_bck),
+                    |match_len| match error_fwd.get() {
+                        Some(pos) if pos < match_len => Action::Stop,
+                        _ => f(match_len * 2),
                     },
                 )
             }
@@ -409,47 +405,54 @@ pub(crate) fn epsilon_closure<C: CodeLoc>(
 /// bytes at a time, returning one of the bytes and making sure that other
 /// is zero. Which of the two bytes is returned and which is zero depends
 /// on the kind of iterator you create. With [`WideIter::non_zero_first`]
-/// you create an iterator that expects the first byte of each pair to be
-/// non-zero, and the second one to be zero.
+/// the iterator expects the first byte of each pair to be non-zero, and
+/// the second one to be zero.
 ///
-/// In the other hand, [`WideIter::zero_first`] expect the first byte of
-/// each pair to be zero, and the second one to be the non-zero byte
-/// returned by the iterator.   
+/// In the other hand, with [`WideIter::zero_first`] the iterator expects
+/// the first byte of each pair to be zero, and the second one to be the
+/// non-zero byte.
+///
+/// When the iterator finds a byte that is expected to be zero, but it's
+/// not, it saves the number of valid pairs that were consumed before
+/// finding this invalid pair.
 ///
 /// ```ignore
-/// let error = Cell::new(false);
+/// let error_pos = Cell::new(None);
 /// let v = vec![1,0,2,0,3,0];
 /// // The non-zero values are expected to be the first of each pair.
-/// let i = WideIter::non_zero_first(v.iter(), &error);
+/// let i = WideIter::non_zero_first(v.iter(), &error_pos);
 /// assert_eq!(i.collect(), vec![1,2,3]);
 /// // No error.
-/// assert!(!error_flag.get());
+/// assert_eq!(error_pos.get(), None);
 /// ```
 ///
 /// ```ignore
-/// let error = Cell::new(false);
+/// let error_pos = Cell::new(None);
 /// let v = vec![1,100,2,0,3,0];
-/// let i = WideIter::non_zero_first(v.iter(), &error);
+/// let i = WideIter::non_zero_first(v.iter(), &error_pos);
 /// assert_eq!(i.collect(), vec![1,2,3]);
-/// // Error! between 1 and 2 there's a non-zero value 100
-/// assert!(error_flag.get());
+/// // Error! the 1 is followed by 100 instead of 0. The
+/// // error position is 0 because no valid pairs were found
+/// // before this error.
+/// assert!(error_pos.get(), Some(0));
 /// ```
 ///
 /// ```ignore
-/// let error = Cell::new(false);
+/// let error_pos = Cell::new(None);
 /// let v = vec![0,1,0,2,0,3];
 /// // The zero values are expected to be the first of each pair.
-/// let i = WideIter::zero_first(v.iter(), &error);
+/// let i = WideIter::zero_first(v.iter(), &error_pos);
 /// assert_eq!(i.collect(), vec![1,2,3]);
 /// // No error
-/// assert!(!error_flag.get());
+/// assert!(error_pos.get(), None);
 /// ```
 struct WideIter<'a, I>
 where
     I: Iterator<Item = &'a u8>,
 {
     iter: I,
-    error_flag: &'a Cell<bool>,
+    error_pos: &'a Cell<Option<usize>>,
+    valid_pairs: usize,
     zero_first: bool,
 }
 
@@ -457,18 +460,18 @@ impl<'a, I> WideIter<'a, I>
 where
     I: Iterator<Item = &'a u8>,
 {
-    pub fn non_zero_first(iter: I, error_flag: &'a Cell<bool>) -> Self
+    pub fn non_zero_first(iter: I, error_pos: &'a Cell<Option<usize>>) -> Self
     where
         I: Iterator<Item = &'a u8>,
     {
-        WideIter { iter, error_flag, zero_first: false }
+        WideIter { iter, error_pos, valid_pairs: 0, zero_first: false }
     }
 
-    pub fn zero_first(iter: I, error_flag: &'a Cell<bool>) -> Self
+    pub fn zero_first(iter: I, error_pos: &'a Cell<Option<usize>>) -> Self
     where
         I: Iterator<Item = &'a u8>,
     {
-        WideIter { iter, error_flag, zero_first: true }
+        WideIter { iter, error_pos, valid_pairs: 0, zero_first: true }
     }
 }
 
@@ -483,14 +486,16 @@ where
         let second_byte = self.iter.next()?;
 
         if self.zero_first {
-            if *first_byte != 0_u8 {
-                self.error_flag.replace(true);
+            if *first_byte != 0_u8 && self.error_pos.get().is_none() {
+                self.error_pos.set(Some(self.valid_pairs));
             }
+            self.valid_pairs += 1;
             Some(second_byte)
         } else {
-            if *second_byte != 0_u8 {
-                self.error_flag.replace(true);
+            if *second_byte != 0_u8 && self.error_pos.get().is_none() {
+                self.error_pos.set(Some(self.valid_pairs));
             }
+            self.valid_pairs += 1;
             Some(first_byte)
         }
     }

lib/src/tests/mod.rs
@@ -1481,6 +1481,12 @@ fn regexp_wide() {
         b"f\x00o\x00o\x00b\x00a\x00r\x00b\x00a\x00z\x00"
     );
 
+    pattern_match!(
+        r#"/https?:\/\/.{5,128}\.png/ wide"#,
+        b"\xcc\xcch\x00t\x00t\x00p\x00s\x00:\x00/\x00/\x00f\x00o\x00o\x00b\x00a\x00r\x00/\x00b\x00a\x00z\x00.\x00p\x00n\x00g\x00\xcc\xcc",
+        b"h\x00t\x00t\x00p\x00s\x00:\x00/\x00/\x00f\x00o\x00o\x00b\x00a\x00r\x00/\x00b\x00a\x00z\x00.\x00p\x00n\x00g\x00"
+    );
+
     pattern_false!(
         r#"/foobar.[A-Z]{1}/ wide"#,
         b"f\x00o\x00o\x00b\x00a\x00r\x00\x00\x15W\x00"