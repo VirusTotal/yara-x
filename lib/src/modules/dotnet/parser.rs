VirusTotal/yara-x

commit 981eb9ca9158bb4f665861f84e8ce185e6b04553

fix: issue while parsing PE resources.
While reading PE resources we were relying on the size specified in the directory entry (`IMAGE_DIRECTORY_ENTRY_RESOURCE`). However, some files like `8970b83310e030f7b7191ed1329433bcf8a4f574e66f2d39bca5a4360c1dac96` may have an incorrect size in the directory entry and still have valid resources that are parsed by both YARA and `pefile`.

lib/src/modules/pe/parser.rs
@@ -311,9 +311,10 @@ impl<'a> PE<'a> {
 
     /// Returns the RVA, size and data associated to a given directory entry.
     ///
-    /// This function translates the RVA into a file offset and returns the
-    /// chunk of file that starts at that offset and has the size indicated by
-    /// the directory entry.
+    /// The returned tuple is `(rva, size, data)`. This function converts the
+    /// RVA into a file offset, the `data` slice contains the file content from
+    /// that offset to the end of the file. Notice that the size indicated by
+    /// the directory entry is ignored, because sometimes it is wrong.
     ///
     /// Returns `None` if the PE is corrupted in some way that prevents the
     /// data from being found.
@@ -325,9 +326,9 @@ impl<'a> PE<'a> {
         // than MAX_DIR_ENTRIES.
         debug_assert!(index < Self::MAX_DIR_ENTRIES);
 
-        // In theory, `index` should be be lower than
-        // `number_of_rva_and_sizes`, but we don't enforce it because some PE
-        // files have a `number_of_rva_and_sizes` values lower than the actual
+        // In theory, `index` should be lower than `number_of_rva_and_sizes`,
+        // however, we don't enforce it because some PE files have a
+        // `number_of_rva_and_sizes` values lower than the actual
         // number of directory entries. For example, the .NET file
         // 7ff1bf680c80fd73c0b35084904848b3705480ddeb6d0eff62180bd14cd18570
         // has `number_of_rva_and_sizes` set to 11, but it has a valid
@@ -343,12 +344,7 @@ impl<'a> PE<'a> {
             .map(|(_reminder, entry)| entry)?;
 
         let start = self.rva_to_offset(dir_entry.addr)? as usize;
-        let end = min(
-            self.data.len(),
-            start.saturating_add(dir_entry.size as usize),
-        );
-
-        let data = self.data.get(start..end)?;
+        let data = self.data.get(start..)?;
 
         Some((dir_entry.addr, dir_entry.size, data))
     }
@@ -1487,8 +1483,13 @@ impl<'a> PE<'a> {
 
     /// Parses PE imports.
     fn parse_imports(&self) -> Option<Vec<(&'a str, Vec<ImportedFunc>)>> {
-        let (_, _, import_data) =
+        let (rva, _, import_data) =
             self.get_dir_entry_data(Self::IMAGE_DIRECTORY_ENTRY_IMPORT)?;
+
+        if rva == 0 {
+            return None;
+        }
+
         self.parse_import_impl(import_data, Self::parse_import_descriptor)
     }
 
@@ -1496,8 +1497,13 @@ impl<'a> PE<'a> {
     fn parse_delayed_imports(
         &self,
     ) -> Option<Vec<(&'a str, Vec<ImportedFunc>)>> {
-        let (_, _, import_data) =
+        let (rva, _, import_data) =
             self.get_dir_entry_data(Self::IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT)?;
+
+        if rva == 0 {
+            return None;
+        }
+
         self.parse_import_impl(import_data, Self::parse_delay_load_descriptor)
     }
 
@@ -1746,6 +1752,10 @@ impl<'a> PE<'a> {
     fn parse_exports(&self) -> Option<ExportInfo<'a>> {
         let (exports_rva, exports_size, exports_data) =
             self.get_dir_entry_data(Self::IMAGE_DIRECTORY_ENTRY_EXPORT)?;
+
+        if exports_rva == 0 {
+            return None;
+        }
 
         // Parse the IMAGE_EXPORT_DIRECTORY structure.
         let (_, exports) = Self::parse_exports_dir_entry(exports_data).ok()?;

commit ca1ece9e3dcf82f481596d641c04d8a46981c80a

fix: bug while parsing .NET file.
File `55e4ce3fe726043070ecd7de5a74b2459ea8bed19ef2a36ce7884b2ab0863047` was considered a valid .NET file, even if the `IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR` had size 0. This was a regression introduced in 981eb9c.

lib/src/modules/dotnet/parser.rs
@@ -128,7 +128,7 @@ impl<'a> Dotnet<'a> {
         let pe = PE::parse(data)?;
 
         let (_, _, cli_header) = pe
-            .get_dir_entry_data(PE::IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR)
+            .get_dir_entry_data(PE::IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR, true)
             .ok_or(Error::InvalidDotNet)?;
 
         let (_, cli_header) = Self::parse_cli_header(cli_header)?;

lib/src/modules/pe/parser.rs
@@ -311,16 +311,15 @@ impl<'a> PE<'a> {
 
     /// Returns the RVA, size and data associated to a given directory entry.
     ///
-    /// The returned tuple is `(rva, size, data)`. This function converts the
-    /// RVA into a file offset, the `data` slice contains the file content from
-    /// that offset to the end of the file. Notice that the size indicated by
-    /// the directory entry is ignored, because sometimes it is wrong.
-    ///
-    /// Returns `None` if the PE is corrupted in some way that prevents the
-    /// data from being found.
+    /// The returned tuple is `(rva, size, data)`, where `rva` and `size` are
+    /// the ones indicated in the directory entry, and `data` is a slice that
+    /// contains the file's content from that RVA to the end of the file if
+    /// `strict_size` is false. Otherwise, the slice will be limited to the
+    /// size indicated in the directory entry.
     pub fn get_dir_entry_data(
         &self,
         index: usize,
+        strict_size: bool,
     ) -> Option<(u32, u32, &'a [u8])> {
         // Nobody should call this function with an index greater
         // than MAX_DIR_ENTRIES.
@@ -344,7 +343,14 @@ impl<'a> PE<'a> {
             .map(|(_reminder, entry)| entry)?;
 
         let start = self.rva_to_offset(dir_entry.addr)? as usize;
-        let data = self.data.get(start..)?;
+
+        let end = if strict_size {
+            min(self.data.len(), start.saturating_add(dir_entry.size as usize))
+        } else {
+            self.data.len()
+        };
+
+        let data = self.data.get(start..end)?;
 
         Some((dir_entry.addr, dir_entry.size, data))
     }
@@ -1223,8 +1229,8 @@ impl<'a> PE<'a> {
     /// individual resources of that type, and the children of each individual
     /// resource represent the resource in a specific language.
     fn parse_resources(&self) -> Option<(ResourceDir, Vec<Resource<'a>>)> {
-        let (_, _, rsrc_section) =
-            self.get_dir_entry_data(Self::IMAGE_DIRECTORY_ENTRY_RESOURCE)?;
+        let (_, _, rsrc_section) = self
+            .get_dir_entry_data(Self::IMAGE_DIRECTORY_ENTRY_RESOURCE, false)?;
 
         let mut queue = VecDeque::new();
         let mut resources = vec![];
@@ -1346,7 +1352,7 @@ impl<'a> PE<'a> {
     /// Parses the PE debug information and extracts the PDB path.
     fn parse_dbg(&self) -> Option<&'a str> {
         let (_, _, dbg_section) =
-            self.get_dir_entry_data(Self::IMAGE_DIRECTORY_ENTRY_DEBUG)?;
+            self.get_dir_entry_data(Self::IMAGE_DIRECTORY_ENTRY_DEBUG, true)?;
 
         let entries = many0(Self::parse_dbg_dir_entry)(dbg_section)
             .map(|(_, entries)| entries)
@@ -1484,7 +1490,7 @@ impl<'a> PE<'a> {
     /// Parses PE imports.
     fn parse_imports(&self) -> Option<Vec<(&'a str, Vec<ImportedFunc>)>> {
         let (rva, _, import_data) =
-            self.get_dir_entry_data(Self::IMAGE_DIRECTORY_ENTRY_IMPORT)?;
+            self.get_dir_entry_data(Self::IMAGE_DIRECTORY_ENTRY_IMPORT, true)?;
 
         if rva == 0 {
             return None;
@@ -1497,8 +1503,10 @@ impl<'a> PE<'a> {
     fn parse_delayed_imports(
         &self,
     ) -> Option<Vec<(&'a str, Vec<ImportedFunc>)>> {
-        let (rva, _, import_data) =
-            self.get_dir_entry_data(Self::IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT)?;
+        let (rva, _, import_data) = self.get_dir_entry_data(
+            Self::IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT,
+            true,
+        )?;
 
         if rva == 0 {
             return None;
@@ -1751,7 +1759,7 @@ impl<'a> PE<'a> {
 
     fn parse_exports(&self) -> Option<ExportInfo<'a>> {
         let (exports_rva, exports_size, exports_data) =
-            self.get_dir_entry_data(Self::IMAGE_DIRECTORY_ENTRY_EXPORT)?;
+            self.get_dir_entry_data(Self::IMAGE_DIRECTORY_ENTRY_EXPORT, true)?;
 
         if exports_rva == 0 {
             return None;