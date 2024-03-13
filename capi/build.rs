96da1750826e81e889dd26cc28ee60953ff28e83
Victor M. Alvarez <vmalvarez@virustotal.com>
2024-03-13 11:27:20 +0100

    chore: use `vcpkg` for building `libmagic` and its dependencies.

--- capi/build.rs
+++ capi/build.rs
@@ -1,4 +1,5 @@
 use std::env;
+use vcpkg;
 
 fn main() {
     println!("cargo:rerun-if-changed=src");
@@ -6,6 +7,10 @@ fn main() {
 
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let output_file = "include/yara-x.h".to_owned();
+
+    vcpkg::find_package("libmagic").unwrap();
+    vcpkg::find_package("bzip2").unwrap();
+    vcpkg::find_package("zlib").unwrap();
 
     match cbindgen::generate(crate_dir) {
         Ok(header) => {
--- cli/build.rs
+++ cli/build.rs
@@ -1,7 +1,9 @@
+use vcpkg;
+
 fn main() {
+    println!("cargo:rerun-if-changed=src");
+
     vcpkg::find_package("libmagic").unwrap();
     vcpkg::find_package("bzip2").unwrap();
     vcpkg::find_package("zlib").unwrap();
-
-    pyo3_build_config::add_extension_module_link_args();
 }
