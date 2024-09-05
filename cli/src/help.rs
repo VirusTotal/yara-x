7cfc9e96f44c77d1d81eff81c6013b058f50480b
Victor M. Alvarez <vmalvarez@virustotal.com>
2024-09-05 15:42:58 +0200

    docs: reformat and reword help text for the `--module-data` option

--- cli/src/help.rs
+++ cli/src/help.rs
@@ -25,14 +25,18 @@ Examples:
 --define some_str=\"foobar\""#;
 
 pub const MODULE_DATA_LONG_HELP: &str = r#"Pass FILE's content as extra data to MODULE
-Some modules require extra data input (besides just the analyzed file) to work. This
-option allows to pass such data to those modules.
-The flag can be used multiple times to pass data to multiple modules.
-The content of the FILE is loaded and interpreted by the module.
+
+Some modules require supplementary data to work, in addition to the scanned file. This
+option allows you to provide that extra data. The flag can be used multiple times to
+supply data to different modules. The content of the FILE is loaded and interpreted by
+the respective module.
+
 Examples:
+
 --module-data=mymodule0=./example0.json --module-data=mymodule1=./example1.json
-such invocation will result in passing the content of `example0.json`, `example1.json`
-to `mymodule0`, `mymodule1` respectively."#;
+
+In this example, the contents of example0.json and example1.json will be passed to
+mymodule0 and mymodule1, respectively."#;
 
 pub const DEPTH_LONG_HELP: &str = r#"Walk directories recursively up to a given depth
 
