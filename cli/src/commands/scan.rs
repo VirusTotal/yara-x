VirusTotal/yara-x

commit 7fffb58b6bf6be555959de28523320d51e4b93f3

fix(cli): print paths relative to the target path.
When the target path passed to the CLI is a relative one, file paths printed by the CLI are relative to the target path.

Closes #212

cli/src/commands/scan.rs
@@ -243,6 +243,8 @@ pub fn exec_scan(args: &ArgMatches) -> anyhow::Result<()> {
         walk::ParWalker::path(target_path)
     };
 
+    let canonical_target_path = target_path.canonicalize()?;
+
     if let Some(num_threads) = num_threads {
         w.num_threads(*num_threads);
     }
@@ -305,11 +307,20 @@ pub fn exec_scan(args: &ArgMatches) -> anyhow::Result<()> {
 
             let now = Instant::now();
 
+            // When the target path passed in the command line is an absolute
+            // path, all file paths are printed as absolute paths, if not, they
+            // are printed as paths relative to the target path.
+            let printable_path = if target_path.is_absolute() {
+                file_path.as_path()
+            } else {
+                file_path.strip_prefix(&canonical_target_path)?
+            };
+
             state
                 .files_in_progress
                 .lock()
                 .unwrap()
-                .push((file_path.clone(), now));
+                .push((printable_path.to_path_buf(), now));
 
             let scan_options = all_metadata.iter().fold(
                 ScanOptions::new(),
@@ -326,11 +337,15 @@ pub fn exec_scan(args: &ArgMatches) -> anyhow::Result<()> {
                 .files_in_progress
                 .lock()
                 .unwrap()
-                .retain(|(p, _)| !file_path.eq(p));
+                .retain(|(p, _)| !printable_path.eq(p));
 
             let scan_results = scan_results?;
-            let matched_count =
-                process_scan_results(args, &file_path, &scan_results, output);
+            let matched_count = process_scan_results(
+                args,
+                printable_path,
+                &scan_results,
+                output,
+            );
 
             state.num_scanned_files.fetch_add(1, Ordering::Relaxed);
             if matched_count > 0 {

commit 2250122fa6eb89f4001bbb1c55eb6ca764b588ef

fix: error introduced in 7fffb58 while scanning a single file.

cli/src/commands/scan.rs
@@ -259,8 +259,6 @@ pub fn exec_scan(args: &ArgMatches) -> anyhow::Result<()> {
         walk::ParWalker::path(target_path)
     };
 
-    let canonical_target_path = target_path.canonicalize()?;
-
     if let Some(num_threads) = num_threads {
         w.num_threads(*num_threads);
     }
@@ -325,20 +323,11 @@ pub fn exec_scan(args: &ArgMatches) -> anyhow::Result<()> {
 
             let now = Instant::now();
 
-            // When the target path passed in the command line is an absolute
-            // path, all file paths are printed as absolute paths, if not, they
-            // are printed as paths relative to the target path.
-            let printable_path = if target_path.is_absolute() {
-                file_path.as_path()
-            } else {
-                file_path.strip_prefix(&canonical_target_path)?
-            };
-
             state
                 .files_in_progress
                 .lock()
                 .unwrap()
-                .push((printable_path.to_path_buf(), now));
+                .push((file_path.to_path_buf(), now));
 
             let scan_options = all_metadata.iter().fold(
                 ScanOptions::new(),
@@ -355,12 +344,12 @@ pub fn exec_scan(args: &ArgMatches) -> anyhow::Result<()> {
                 .files_in_progress
                 .lock()
                 .unwrap()
-                .retain(|(p, _)| !printable_path.eq(p));
+                .retain(|(p, _)| !file_path.eq(p));
 
             let scan_results = scan_results?;
             let matched_count = process_scan_results(
                 args,
-                printable_path,
+                file_path.as_path(),
                 &scan_results,
                 output,
             );

cli/src/walk.rs
@@ -199,18 +199,11 @@ impl<'a> Walker<'a> {
         F: FnMut(&Path) -> anyhow::Result<()>,
         E: FnMut(anyhow::Error) -> anyhow::Result<()>,
     {
-        let path = match self.path.canonicalize() {
-            Ok(path) => path,
-            Err(err) => {
-                return e(err.into());
-            }
-        };
-
         let mut builder = if self.filters.is_empty() {
-            globwalk::GlobWalkerBuilder::from_patterns(path, &["**"])
+            globwalk::GlobWalkerBuilder::from_patterns(self.path, &["**"])
         } else {
             globwalk::GlobWalkerBuilder::from_patterns(
-                path,
+                self.path,
                 self.filters.iter().as_ref(),
             )
         };