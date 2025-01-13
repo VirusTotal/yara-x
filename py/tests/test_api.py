e76001566cc57af769a96007bc6f615708566f41
Jacob Latonis <jlatonis@me.com>
2025-01-13 04:48:17 -0500

    feat: implement the Formatter API for Python lib (#277)
    The Rust API includes support for source code formatting, add it to the Python API too.

--- py/tests/test_api.py
+++ py/tests/test_api.py
@@ -268,3 +268,13 @@ def test_console_log():
   scanner.console_log(callback)
   scanner.scan(b'')
   assert ok
+
+def test_format():
+    import io
+    expected_output = "rule test {\n  condition:\n    true\n}\n"
+    inp = io.StringIO("rule test {condition: true}")
+    output = io.StringIO()
+    fmt = yara_x.Formatter()
+    fmt.format(inp, output)
+    result = output.getvalue()
+    assert result == expected_output
