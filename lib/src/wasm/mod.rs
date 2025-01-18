c585d735105cb626bb2e133c33050b77599200ae
Victor M. Alvarez <vmalvarez@virustotal.com>
2025-01-18 16:15:19 +0100

    chore: reduce WASM memory reservation from 2GB to 256MB.

--- lib/src/wasm/mod.rs
+++ lib/src/wasm/mod.rs
@@ -720,7 +720,8 @@ lazy_static! {
         config.native_unwind_info(false);
 
         config.cranelift_opt_level(wasmtime::OptLevel::SpeedAndSize);
-        config.memory_reservation(0x80000000); // 2GB
+        config.memory_reservation(0x10000000); // 256MB
+        config.memory_reservation_for_growth(0);
         config.guard_before_linear_memory(false);
         config.memory_may_move(false);
         config.epoch_interruption(true);
