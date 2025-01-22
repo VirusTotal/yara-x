9bc6d35e8a1ddfa98b2404d2f85e74dcc5823b1a
Victor M. Alvarez <vmalvarez@virustotal.com>
2025-01-22 13:45:54 +0100

    style: removing trailing spaces.

--- lib/src/compiler/emit.rs
+++ lib/src/compiler/emit.rs
@@ -640,11 +640,11 @@ fn emit_expr(
             if let Some(obj) = func_call.object {
                 emit_expr(ctx, ir, obj, instr);
             }
-            
+
             for expr in func_call.args.iter() {
                 emit_expr(ctx, ir, *expr, instr);
             }
-            
+
             if func_call.signature().result_may_be_undef {
                 emit_call_and_handle_undef(
                     ctx,
