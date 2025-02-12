fa6630032c659ce07da204efdc1509a2bdd3d396
Victor M. Alvarez <vmalvarez@virustotal.com>
2025-02-12 17:23:01 +0100

    tests: fix broken test.

--- cli/src/tests/check.rs
+++ cli/src/tests/check.rs
@@ -153,7 +153,7 @@ fn check_rule_name_error() {
         .success()
         .stdout(
             r#"[ FAIL ] src/tests/testdata/foo.yar
-error[E038]: rule name does not match regex `APT_.+`
+error[E039]: rule name does not match regex `APT_.+`
  --> src/tests/testdata/foo.yar:1:6
   |
 1 | rule foo : bar baz {
