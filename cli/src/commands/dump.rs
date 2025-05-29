09b27a0e7876fa0adf8b50c00251f0595d29c758
Victor M. Alvarez <vmalvarez@virustotal.com>
2025-05-29 16:04:02 +0200

    fix(cli): don't call `expect` when serializing module outputs to YAML and JSON.
    When the program output is redirect to a program like `more` and you abort it before reading the whole output, a "broken pipe" error will be returned and we don't like a panic on such cases.

--- cli/src/commands/dump.rs
+++ cli/src/commands/dump.rs
@@ -138,16 +138,14 @@ pub fn exec_dump(args: &ArgMatches) -> anyhow::Result<()> {
             let mut serializer = JsonSerializer::new(stdout());
             serializer
                 .with_colors(use_color)
-                .serialize(module_output.as_ref())
-                .expect("Failed to serialize");
+                .serialize(module_output.as_ref())?;
             println!();
         }
         Some(OutputFormats::Yaml) | None => {
             let mut serializer = YamlSerializer::new(stdout());
             serializer
                 .with_colors(use_color)
-                .serialize(module_output.as_ref())
-                .expect("Failed to serialize");
+                .serialize(module_output.as_ref())?;
             println!();
         }
     }
