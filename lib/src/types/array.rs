54cff37a246ad134f7148dbc1632792da7e252f4
Victor M. Alvarez <vmalvarez@virustotal.com>
2025-06-10 11:44:02 +0200

    chore: disable clippy warnings.

--- lib/src/types/array.rs
+++ lib/src/types/array.rs
@@ -18,8 +18,11 @@ pub(crate) enum Array {
 }
 
 impl Array {
+    #[allow(clippy::declare_interior_mutable_const)]
     const BUILTIN_METHODS: OnceCell<Rc<SymbolTable>> = OnceCell::new();
+
     pub fn builtin_methods(&self) -> Rc<SymbolTable> {
+        #[allow(clippy::borrow_interior_mutable_const)]
         Self::BUILTIN_METHODS
             .get_or_init(|| {
                 let mut s = SymbolTable::new();
--- lib/src/types/map.rs
+++ lib/src/types/map.rs
@@ -31,8 +31,11 @@ pub(crate) enum Map {
 }
 
 impl Map {
+    #[allow(clippy::declare_interior_mutable_const)]
     const BUILTIN_METHODS: OnceCell<Rc<SymbolTable>> = OnceCell::new();
+
     pub fn builtin_methods(&self) -> Rc<SymbolTable> {
+        #[allow(clippy::borrow_interior_mutable_const)]
         Self::BUILTIN_METHODS
             .get_or_init(|| {
                 let mut s = SymbolTable::new();
