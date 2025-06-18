VirusTotal/yara-x

commit a5a1aaa30c7ec53e7a6876b7a62083b038c60fc8

feat: better module error handling and error propagation (#368)
I noticed that if error occurs in a module (e. g. caused by some syntax error in its (meta)data) it does not propagate the error to the user, but it just doesn't hit anything. In my opinion, it can be very misleading during the debugging process.

Therefore, I've got an idea to change the return type of module `MainFn` to `Result<Box<dyn MessageDyn>, ModuleError>`, which allow to return a string with description of an error which appeared inside a module.

For example if there is missing `,` in cuckoo JSON report we now get:
```
$ ./target/debug/yr scan -x cuckoo=cuckoo.json test.yar test.yar
error:  scanning "test.yar": error un module `cuckoo`: invalid metadata: expected `,` or `}` at line 6 column 5
────────────────────────────────────────────────────────────────────────────────────────────────────────
 0 file(s) scanned in 0.3s. 0 file(s) matched.
```

---------

Co-authored-by: Victor M. Alvarez <vmalvarez@virustotal.com>

lib/src/modules/elf/mod.rs
@@ -27,10 +27,12 @@ thread_local!(
 );
 
 #[module_main]
-fn main(data: &[u8], _meta: Option<&[u8]>) -> ELF {
+fn main(data: &[u8], _meta: Option<&[u8]>) -> Result<ELF, ModuleError> {
     IMPORT_MD5_CACHE.with(|cache| *cache.borrow_mut() = None);
     TLSH_CACHE.with(|cache| *cache.borrow_mut() = None);
-    parser::ElfParser::new().parse(data).unwrap_or_else(|_| ELF::new())
+    parser::ElfParser::new()
+        .parse(data)
+        .map_err(|e| ModuleError::InternalError { err: e.to_string() })
 }
 
 #[module_export]

lib/src/modules/lnk/mod.rs
@@ -17,10 +17,8 @@ use crate::modules::protos::lnk::*;
 pub mod parser;
 
 #[module_main]
-fn main(data: &[u8], _meta: Option<&[u8]>) -> Lnk {
-    parser::LnkParser::new().parse(data).unwrap_or_else(|_| {
-        let mut lnk = Lnk::new();
-        lnk.is_lnk = Some(false);
-        lnk
-    })
+fn main(data: &[u8], _meta: Option<&[u8]>) -> Result<Lnk, ModuleError> {
+    parser::LnkParser::new()
+        .parse(data)
+        .map_err(|e| ModuleError::InternalError { err: e.to_string() })
 }

lib/src/modules/cuckoo/mod.rs
@@ -26,27 +26,26 @@ fn set_local(value: schema::CuckooJson) {
 }
 
 #[module_main]
-fn main(_data: &[u8], meta: Option<&[u8]>) -> Cuckoo {
-    let parsed = match meta {
-        None => Ok(schema::CuckooJson::default()),
-        Some(m) => serde_json::from_slice::<schema::CuckooJson>(m)
+fn main(_data: &[u8], meta: Option<&[u8]>) -> Result<Cuckoo, ModuleError> {
+    let meta = match meta {
+        None | Some([]) => {
+            set_local(schema::CuckooJson::default());
+            return Ok(Cuckoo::new());
+        }
+        Some(meta) => meta,
     };
 
-    match parsed {
+    match serde_json::from_slice::<schema::CuckooJson>(meta) {
         Ok(parsed) => {
             set_local(parsed);
         }
-        #[cfg(feature = "logging")]
         Err(e) => {
-            error!("can't parse cuckoo report: {}", e);
-        }
-        #[cfg(not(feature = "logging"))]
-        Err(_) => {
             set_local(schema::CuckooJson::default());
+            return Err(ModuleError::MetadataError { err: e.to_string() });
         }
     };
 
-    Cuckoo::new()
+    Ok(Cuckoo::new())
 }
 
 #[module_export(name = "network.dns_lookup")]

lib/src/modules/dotnet/mod.rs
@@ -9,13 +9,16 @@ use crate::modules::protos::dotnet::*;
 pub mod parser;
 
 #[module_main]
-fn main(data: &[u8], _meta: Option<&[u8]>) -> Dotnet {
+fn main(
+    data: &[u8],
+    _meta: Option<&[u8]>,
+) -> Result<Dotnet, ModuleError> {
     match parser::Dotnet::parse(data) {
-        Ok(dotnet) => dotnet.into(),
+        Ok(dotnet) => Ok(dotnet.into()),
         Err(_) => {
             let mut dotnet = Dotnet::new();
             dotnet.is_dotnet = Some(false);
-            dotnet
+            Ok(dotnet)
         }
     }
 }

lib/src/modules/hash/mod.rs
@@ -29,7 +29,10 @@ thread_local!(
 );
 
 #[module_main]
-fn main(_data: &[u8], _meta: Option<&[u8]>) -> Hash {
+fn main(
+    _data: &[u8],
+    _meta: Option<&[u8]>,
+) -> Result<Hash, ModuleError> {
     // With every scanned file the cache must be cleared.
     SHA256_CACHE.with(|cache| cache.borrow_mut().clear());
     SHA1_CACHE.with(|cache| cache.borrow_mut().clear());
@@ -37,7 +40,7 @@ fn main(_data: &[u8], _meta: Option<&[u8]>) -> Hash {
     CRC32_CACHE.with(|cache| cache.borrow_mut().clear());
     CHECKSUM32_CACHE.with(|cache| cache.borrow_mut().clear());
 
-    Hash::new()
+    Ok(Hash::new())
 }
 
 #[module_export(name = "md5")]

lib/src/modules/macho/mod.rs
@@ -579,7 +579,7 @@ fn sym_hash(ctx: &mut ScanContext) -> Option<RuntimeString> {
 }
 
 #[module_main]
-fn main(data: &[u8], _meta: Option<&[u8]>) -> Macho {
+fn main(data: &[u8], _meta: Option<&[u8]>) -> Result<Macho, ModuleError> {
     DYLIB_MD5_CACHE.with(|cache| *cache.borrow_mut() = None);
     ENTITLEMENT_MD5_CACHE.with(|cache| *cache.borrow_mut() = None);
     EXPORT_MD5_CACHE.with(|cache| *cache.borrow_mut() = None);
@@ -587,7 +587,7 @@ fn main(data: &[u8], _meta: Option<&[u8]>) -> Macho {
     SYM_MD5_CACHE.with(|cache| *cache.borrow_mut() = None);
 
     match parser::MachO::parse(data) {
-        Ok(macho) => macho.into(),
-        Err(_) => Macho::new(),
+        Ok(macho) => Ok(macho.into()),
+        Err(_) => Ok(Macho::new()),
     }
 }

lib/src/modules/magic/mod.rs
@@ -28,12 +28,12 @@ thread_local! {
 }
 
 #[module_main]
-fn main(_data: &[u8], _meta: Option<&[u8]>) -> Magic {
+fn main(_data: &[u8], _meta: Option<&[u8]>) -> Result<Magic, ModuleError> {
     // With every scanned file the cache must be cleared.
     TYPE_CACHE.set(None);
     MIME_TYPE_CACHE.set(None);
 
-    Magic::new()
+    Ok(Magic::new())
 }
 
 #[module_export(name = "type")]

lib/src/modules/mod.rs
@@ -3,6 +3,8 @@ use protobuf::MessageDyn;
 use rustc_hash::FxHashMap;
 use std::sync::LazyLock;
 
+use thiserror::Error;
+
 pub mod protos {
     include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
 }
@@ -26,8 +28,20 @@ pub(crate) mod prelude {
 
 include!("modules.rs");
 
+/// Enum describing errors occurred in modules.
+#[derive(Error, Debug)]
+pub enum ModuleError {
+    /// Invalid format of module metadata.
+    #[error("invalid metadata: {err}")]
+    MetadataError { err: String },
+    /// Error occurred when processing the input data.
+    #[error("internal error: {err}")]
+    InternalError { err: String },
+}
+
 /// Type of module's main function.
-type MainFn = fn(&[u8], Option<&[u8]>) -> Box<dyn MessageDyn>;
+type MainFn =
+    fn(&[u8], Option<&[u8]>) -> Result<Box<dyn MessageDyn>, ModuleError>;
 
 /// Describes a YARA module.
 pub(crate) struct Module {
@@ -250,7 +264,7 @@ pub mod mods {
                 module.root_struct_descriptor.full_name() == proto_name
             })?;
 
-        Some(module.main_fn?(data, meta))
+        module.main_fn?(data, meta).ok()
     }
 
     /// Invoke all YARA modules and return the data produced by them.

commit ae9a6323ff5e6725fac69b76997db82aa53e713a

fix: regression introduced in #368.
Closes #378 and #379.

lib/src/modules/elf/mod.rs
@@ -30,9 +30,11 @@ thread_local!(
 fn main(data: &[u8], _meta: Option<&[u8]>) -> Result<ELF, ModuleError> {
     IMPORT_MD5_CACHE.with(|cache| *cache.borrow_mut() = None);
     TLSH_CACHE.with(|cache| *cache.borrow_mut() = None);
-    parser::ElfParser::new()
-        .parse(data)
-        .map_err(|e| ModuleError::InternalError { err: e.to_string() })
+
+    match parser::ElfParser::new().parse(data) {
+        Ok(elf) => Ok(elf),
+        Err(_) => Ok(ELF::new()),
+    }
 }
 
 #[module_export]

lib/src/modules/lnk/mod.rs
@@ -18,7 +18,12 @@ pub mod parser;
 
 #[module_main]
 fn main(data: &[u8], _meta: Option<&[u8]>) -> Result<Lnk, ModuleError> {
-    parser::LnkParser::new()
-        .parse(data)
-        .map_err(|e| ModuleError::InternalError { err: e.to_string() })
+    match parser::LnkParser::new().parse(data) {
+        Ok(lnk) => Ok(lnk),
+        Err(_) => {
+            let mut lnk = Lnk::new();
+            lnk.is_lnk = Some(false);
+            Ok(lnk)
+        }
+    }
 }

lib/src/modules/dotnet/tests/testdata/e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855.out
@@ -1,0 +1,1 @@
+is_dotnet: false

lib/src/modules/lnk/tests/testdata/lnk-empty.out
@@ -1,0 +1,1 @@
+is_lnk: false