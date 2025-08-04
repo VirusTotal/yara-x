dc15d815737565765bee26cb92c8621dbf375717
Michal Krulich <michal.krulich@gendigital.com>
2025-08-04 16:42:56 +0200

    fix(cuckoo): return `0` instead of `undefined` by default

--- lib/src/modules/cuckoo/mod.rs
+++ lib/src/modules/cuckoo/mod.rs
@@ -46,220 +46,202 @@ fn main(_data: &[u8], meta: Option<&[u8]>) -> Result<Cuckoo, ModuleError> {
 }
 
 #[module_export(name = "network.dns_lookup")]
-fn network_dns_lookup_r(
-    ctx: &ScanContext,
-    regexp_id: RegexpId,
-) -> Option<i64> {
-    Some(
-        get_local()?
-            .network
-            .as_ref()?
-            .domains
-            .iter()
-            .flatten()
-            .filter(|domain| {
-                matches!(&domain.domain, Some(domain_domain) if ctx.regexp_matches(regexp_id, domain_domain.as_bytes()))
-            })
-            .count() as _,
-    )
+fn network_dns_lookup_r(ctx: &ScanContext, regexp_id: RegexpId) -> i64 {
+    get_local()
+        .as_ref()
+        .and_then(|local| local.network.as_ref())
+        .and_then(|network| network.domains.as_ref())
+        .map(|domains| {
+            domains
+                .iter()
+                .filter(|domain| {
+                    matches!(&domain.domain, Some(domain_domain) if ctx.regexp_matches(regexp_id, domain_domain.as_bytes()))
+                })
+                .count() as i64
+        })
+        .unwrap_or(0)
 }
 
 #[module_export(name = "network.http_request")]
-fn network_http_request_r(
-    ctx: &ScanContext,
-    regexp_id: RegexpId,
-) -> Option<i64> {
-    Some(
-        get_local()?
-            .network
-            .as_ref()?
-            .http
-            .iter()
-            .flatten()
-            .filter(|http| {
-                http.method.is_some() // ~> is request (is not response)
-                    && matches!(&http.uri, Some(uri) if ctx.regexp_matches(regexp_id, uri.as_bytes()))
-            })
-            .count() as _,
-    )
+fn network_http_request_r(ctx: &ScanContext, regexp_id: RegexpId) -> i64 {
+    get_local()
+        .as_ref()
+        .and_then(|local| local.network.as_ref())
+        .and_then(|network| network.http.as_ref())
+        .map(|http| {
+            http.iter()
+                .filter(|http| {
+                    http.method.is_some() // ~> is request (is not response)
+                        && matches!(&http.uri, Some(uri) if ctx.regexp_matches(regexp_id, uri.as_bytes()))
+                })
+                .count() as i64
+        })
+        .unwrap_or(0)
 }
 
 #[module_export(name = "network.http_get")]
-fn network_http_get_r(ctx: &ScanContext, regexp_id: RegexpId) -> Option<i64> {
-    Some(
-        get_local()?
-            .network
-            .as_ref()?
-            .http
-            .iter()
-            .flatten()
-            .filter(|http| {
-                http.method
-                    .as_ref()
-                    .map(|method| method.eq_ignore_ascii_case("get"))
-                    .unwrap_or(false)
-                    && matches!(&http.uri, Some(uri) if ctx.regexp_matches(regexp_id, uri.as_bytes()))
-            })
-            .count() as _,
-    )
+fn network_http_get_r(ctx: &ScanContext, regexp_id: RegexpId) -> i64 {
+    get_local()
+        .as_ref()
+        .and_then(|local| local.network.as_ref())
+        .and_then(|network| network.http.as_ref())
+        .map(|http| {
+            http.iter()
+                .filter(|http| {
+                    http.method
+                        .as_ref()
+                        .map(|method| method.eq_ignore_ascii_case("get"))
+                        .unwrap_or(false)
+                        && matches!(&http.uri, Some(uri) if ctx.regexp_matches(regexp_id, uri.as_bytes()))
+                })
+                .count() as i64
+        })
+        .unwrap_or(0)
 }
 
 #[module_export(name = "network.http_post")]
-fn network_http_post_r(ctx: &ScanContext, regexp_id: RegexpId) -> Option<i64> {
-    Some(
-        get_local()?
-            .network
-            .as_ref()?
-            .http
-            .iter()
-            .flatten()
-            .filter(|http| {
-                http.method
-                    .as_ref()
-                    .map(|method| method.eq_ignore_ascii_case("post"))
-                    .unwrap_or(false)
-                    && matches!(&http.uri, Some(uri) if ctx.regexp_matches(regexp_id, uri.as_bytes()))
-            })
-            .count() as _,
-    )
+fn network_http_post_r(ctx: &ScanContext, regexp_id: RegexpId) -> i64 {
+    get_local()
+        .as_ref()
+        .and_then(|local| local.network.as_ref())
+        .and_then(|network| network.http.as_ref())
+        .map(|http| {
+            http.iter()
+                .filter(|http| {
+                    http.method
+                        .as_ref()
+                        .map(|method| method.eq_ignore_ascii_case("post"))
+                        .unwrap_or(false)
+                        && matches!(&http.uri, Some(uri) if ctx.regexp_matches(regexp_id, uri.as_bytes()))
+                })
+                .count() as i64
+        })
+        .unwrap_or(0)
 }
 
 #[module_export(name = "network.http_user_agent")]
-fn network_http_user_agent_r(
-    ctx: &ScanContext,
-    regexp_id: RegexpId,
-) -> Option<i64> {
-    Some(
-        get_local()?
-            .network
-            .as_ref()?
-            .http
-            .iter()
-            .flatten()
-            .flat_map(|http| http.user_agent.iter())
-            .filter(|user_agent| {
-                ctx.regexp_matches(regexp_id, user_agent.as_bytes())
-            })
-            .count() as _,
-    )
+fn network_http_user_agent_r(ctx: &ScanContext, regexp_id: RegexpId) -> i64 {
+    get_local()
+        .as_ref()
+        .and_then(|local| local.network.as_ref())
+        .and_then(|network| network.http.as_ref())
+        .map(|http| {
+            http.iter()
+                .flat_map(|http| http.user_agent.iter())
+                .filter(|user_agent| {
+                    ctx.regexp_matches(regexp_id, user_agent.as_bytes())
+                })
+                .count() as i64
+        })
+        .unwrap_or(0)
 }
 
 #[module_export(name = "network.tcp")]
-fn network_tcp_ri(
-    ctx: &ScanContext,
-    dst_re: RegexpId,
-    port: i64,
-) -> Option<i64> {
-    Some(
-        get_local()?
-            .network
-            .as_ref()?
-            .tcp
-            .iter()
-            .flatten()
-            .filter(|tcp|
-                matches!(tcp.dport, Some(dport) if {
-                    dport == port as u64
-                        && tcp
-                            .dst
-                            .iter()
-                            .chain(tcp.dst_domain.iter())
-                            .any(|dst| ctx.regexp_matches(dst_re, dst.as_bytes()))
+fn network_tcp_ri(ctx: &ScanContext, dst_re: RegexpId, port: i64) -> i64 {
+    get_local()
+        .as_ref()
+        .and_then(|local| local.network.as_ref())
+        .and_then(|network| network.tcp.as_ref())
+        .map(|tcp| {
+            tcp.iter()
+                .filter(|tcp| {
+                    matches!(tcp.dport, Some(dport) if {
+                        dport == port as u64
+                            && tcp
+                                .dst
+                                .iter()
+                                .chain(tcp.dst_domain.iter())
+                                .any(|dst| ctx.regexp_matches(dst_re, dst.as_bytes()))
+                    })
                 })
-            )
-            .count() as _,
-    )
+                .count() as i64
+        })
+        .unwrap_or(0)
 }
 
 #[module_export(name = "network.udp")]
-fn network_udp_ri(
-    ctx: &ScanContext,
-    dst_re: RegexpId,
-    port: i64,
-) -> Option<i64> {
-    Some(
-        get_local()?
-            .network
-            .as_ref()?
-            .udp
-            .iter()
-            .flatten()
-            .filter(|udp| matches!(udp.dport, Some(dport) if {
-                dport == port as u64
-                    && udp
-                        .dst
-                        .iter()
-                        .chain(udp.dst_domain.iter())
-                        .any(|dst| ctx.regexp_matches(dst_re, dst.as_bytes()))
-            }))
-            .count() as _,
-    )
+fn network_udp_ri(ctx: &ScanContext, dst_re: RegexpId, port: i64) -> i64 {
+    get_local()
+        .as_ref()
+        .and_then(|local| local.network.as_ref())
+        .and_then(|network| network.udp.as_ref())
+        .map(|udp| {
+            udp.iter()
+                .filter(|udp| matches!(udp.dport, Some(dport) if {
+                    dport == port as u64
+                        && udp
+                            .dst
+                            .iter()
+                            .chain(udp.dst_domain.iter())
+                            .any(|dst| ctx.regexp_matches(dst_re, dst.as_bytes()))
+                }))
+                .count() as i64
+        })
+        .unwrap_or(0)
 }
 
 #[module_export(name = "network.host")]
-fn network_host_r(ctx: &ScanContext, re: RegexpId) -> Option<i64> {
-    Some(
-        get_local()?
-            .network
-            .as_ref()?
-            .hosts
-            .iter()
-            .flatten()
-            .filter(|host| ctx.regexp_matches(re, host.as_bytes()))
-            .count() as _,
-    )
+fn network_host_r(ctx: &ScanContext, re: RegexpId) -> i64 {
+    get_local()
+        .as_ref()
+        .and_then(|local| local.network.as_ref())
+        .and_then(|network| network.hosts.as_ref())
+        .map(|hosts| {
+            hosts
+                .iter()
+                .filter(|host| ctx.regexp_matches(re, host.as_bytes()))
+                .count() as i64
+        })
+        .unwrap_or(0)
 }
 
 #[module_export(name = "sync.mutex")]
-fn sync_mutex_r(ctx: &ScanContext, mutex_re: RegexpId) -> Option<i64> {
-    Some(
-        get_local()?
-            .behavior
-            .as_ref()?
-            .summary
-            .as_ref()?
-            .mutexes
-            .iter()
-            .flatten()
-            .filter(|mutex| ctx.regexp_matches(mutex_re, mutex.as_bytes()))
-            .count() as _,
-    )
+fn sync_mutex_r(ctx: &ScanContext, mutex_re: RegexpId) -> i64 {
+    get_local()
+        .as_ref()
+        .and_then(|local| local.behavior.as_ref())
+        .and_then(|behavior| behavior.summary.as_ref())
+        .map(|summary| {
+            summary
+                .mutexes
+                .iter()
+                .flatten()
+                .filter(|mutex| ctx.regexp_matches(mutex_re, mutex.as_bytes()))
+                .count() as i64
+        })
+        .unwrap_or(0)
 }
 
 #[module_export(name = "filesystem.file_access")]
-fn filesystem_file_access_r(
-    ctx: &ScanContext,
-    regexp_id: RegexpId,
-) -> Option<i64> {
-    Some(
-        get_local()?
-            .behavior
-            .as_ref()?
-            .summary
-            .as_ref()?
-            .files
-            .iter()
-            .flatten()
-            .filter(|file| ctx.regexp_matches(regexp_id, file.as_bytes()))
-            .count() as _,
-    )
+fn filesystem_file_access_r(ctx: &ScanContext, regexp_id: RegexpId) -> i64 {
+    get_local()
+        .as_ref()
+        .and_then(|local| local.behavior.as_ref())
+        .and_then(|behavior| behavior.summary.as_ref())
+        .map(|summary| {
+            summary
+                .files
+                .iter()
+                .flatten()
+                .filter(|file| ctx.regexp_matches(regexp_id, file.as_bytes()))
+                .count() as i64
+        })
+        .unwrap_or(0)
 }
 
 #[module_export(name = "registry.key_access")]
-fn registry_key_access_r(
-    ctx: &ScanContext,
-    regexp_id: RegexpId,
-) -> Option<i64> {
-    Some(
-        get_local()?
-            .behavior
-            .as_ref()?
-            .summary
-            .as_ref()?
-            .keys
-            .iter()
-            .flatten()
-            .filter(|key| ctx.regexp_matches(regexp_id, key.as_bytes()))
-            .count() as _,
-    )
+fn registry_key_access_r(ctx: &ScanContext, regexp_id: RegexpId) -> i64 {
+    get_local()
+        .as_ref()
+        .and_then(|local| local.behavior.as_ref())
+        .and_then(|behavior| behavior.summary.as_ref())
+        .map(|summary| {
+            summary
+                .keys
+                .iter()
+                .flatten()
+                .filter(|key| ctx.regexp_matches(regexp_id, key.as_bytes()))
+                .count() as i64
+        })
+        .unwrap_or(0)
 }
