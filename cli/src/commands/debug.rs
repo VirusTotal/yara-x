14bbae376f060e3aeac2d7ac313173d7147abbca
Wesley Shields <wxs@atarininja.org>
2025-11-09 14:54:26 -0500

    Implement "deps" command.
    Given a set of rules parse it and walk the AST to find identifiers and generate
    a dot file of them that can be fed into graphviz for visualization.
    
    By default it generates a graph of all the rules but you can select any number
    of rules with the -r argument.
    
    For example, given these rules:
    
    ```
    rule a { condition: pe.is_dll() }
    rule b { condition: a }
    rule c { condition: b }
    rule d { condition: false }
    ```
    
    And selecting using `-r b` you get output that looks like this:
    
    ```
    digraph {
      b [fillcolor=paleturquoise, style="filled"];
      a [fillcolor=paleturquoise, style="filled"];
      pe [fillcolor=palegreen, style="filled"];
      a -> pe;
      b -> a;
    }
    ```
    
    This mode is best thought of as "what is the minimum set of rules and imports I
    need to execute the selected rule."
    
    Using the -R argument displays the reverse dependencies of a rule. For the same
    rules above the output when using -R is:
    
    ```
    digraph {
      b [fillcolor=paleturquoise, style="filled"];
      c [fillcolor=paleturquoise, style="filled"];
      c -> b;
    }
    ```
    
    This mode is best thought of as "if I change this rule, what other rules do I
    also need to test."

--- cli/src/commands/debug.rs
+++ cli/src/commands/debug.rs
@@ -1,4 +1,5 @@
 #![cfg(feature = "debug-cmd")]
+use std::collections::{BTreeMap, HashSet};
 use std::fs;
 use std::io::stdout;
 use std::path::PathBuf;
@@ -6,8 +7,10 @@ use std::path::PathBuf;
 use anyhow::Context;
 use clap::{arg, value_parser, ArgAction, ArgMatches, Command};
 
+use dot_writer::{Attributes, Color, DotWriter, Scope, Style};
 use yara_x::SourceCode;
-use yara_x_parser::ast::AST;
+use yara_x_parser::ast::dfs::{DFSEvent, DFSIter};
+use yara_x_parser::ast::{Expr, Quantifier, AST};
 use yara_x_parser::cst::CST;
 use yara_x_parser::Parser;
 
@@ -79,6 +82,25 @@ pub fn modules() -> Command {
     super::command("modules").about("List available modules")
 }
 
+pub fn deps() -> Command {
+    super::command("deps")
+        .about("Show rule dependencies, modules and unknown identifiers")
+        .arg(
+            arg!(<RULES_PATH>)
+                .help("Path to YARA source file")
+                .value_parser(value_parser!(PathBuf)),
+        )
+        .arg(
+            arg!(-r - -"rule")
+                .help("Rules to display information for")
+                .action(ArgAction::Append),
+        )
+        .arg(
+            arg!(-R - -"reverse")
+                .help("Also show reverse dependencies of selected rules"),
+        )
+}
+
 pub fn debug() -> Command {
     super::command("debug")
         .about("Debug utilities")
@@ -88,6 +110,7 @@ pub fn debug() -> Command {
         .subcommand(ir())
         .subcommand(wasm())
         .subcommand(modules())
+        .subcommand(deps())
 }
 
 pub fn exec_debug(args: &ArgMatches, config: &Config) -> anyhow::Result<()> {
@@ -97,6 +120,7 @@ pub fn exec_debug(args: &ArgMatches, config: &Config) -> anyhow::Result<()> {
         Some(("ir", args)) => exec_ir(args, config),
         Some(("wasm", args)) => exec_wasm(args, config),
         Some(("modules", args)) => exec_modules(args, config),
+        Some(("deps", args)) => exec_deps(args, config),
         _ => unreachable!(),
     }
 }
@@ -168,4 +192,237 @@ fn exec_modules(_args: &ArgMatches, _config: &Config) -> anyhow::Result<()> {
         println!("{}", name);
     }
     Ok(())
+}
+
+#[derive(Debug)]
+struct Deps<'a> {
+    rules: HashSet<&'a str>,
+    modules: HashSet<&'a str>,
+    unknowns: HashSet<&'a str>,
+}
+
+fn exec_deps(args: &ArgMatches, _config: &Config) -> anyhow::Result<()> {
+    let rules_path = args.get_one::<PathBuf>("RULES_PATH").unwrap();
+    let requested_rules = args.get_many::<String>("rule");
+    let reverse_deps = args.get_flag("reverse");
+
+    let requested_rules: Vec<_> = requested_rules
+        .map_or(Vec::new(), |v| v.collect())
+        .into_iter()
+        .map(|v| v.as_str())
+        .collect();
+
+    if requested_rules.is_empty() && reverse_deps {
+        println!("Must specify a rule when displaying reverse dependencies.");
+        return Ok(());
+    }
+
+    let src = fs::read(rules_path)
+        .with_context(|| format!("can not read `{}`", rules_path.display()))?;
+
+    let parser = Parser::new(src.as_slice());
+    let ast: AST = parser.into();
+
+    if !ast.errors().is_empty() {
+        for err in ast.errors().iter() {
+            println!("{err:?}");
+        }
+        return Ok(());
+    }
+
+    // Map of rules to dependencies, modules and unknown identifiers they use.
+    //
+    // Given these rules:
+    //
+    // rule a { condition: pe.is_dll() }
+    // rule b { condition: a or x }
+    //
+    // Deps would look like:
+    //
+    // {
+    //  "a": Deps { rules: {}, modules: {"pe"}, unknowns: {} },
+    //  "b": Deps { rules: {"a"}, modules: {}, unknowns: {"x"} }
+    // }
+    let mut dep_map: BTreeMap<&str, Deps> = BTreeMap::new();
+
+    for rule in ast.rules() {
+        dep_map.insert(
+            rule.identifier.name,
+            Deps {
+                rules: HashSet::new(),
+                modules: HashSet::new(),
+                unknowns: HashSet::new(),
+            },
+        );
+        check_expr(&rule.condition, rule.identifier.name, &mut dep_map);
+    }
+
+    let graph = generate_graph(&dep_map, &requested_rules, reverse_deps);
+    println!("{graph}");
+
+    Ok(())
+}
+
+fn generate_graph(
+    dep_map: &BTreeMap<&str, Deps>,
+    requested_rules: &Vec<&str>,
+    reverse_deps: bool,
+) -> String {
+    let mut bytes = Vec::new();
+
+    // Set of created nodes to avoid creating duplicates.
+    let mut nodes: HashSet<&str> = HashSet::new();
+    {
+        let mut writer = DotWriter::from(&mut bytes);
+        let mut graph = writer.digraph();
+        for (rule, deps) in dep_map.iter() {
+            if (!requested_rules.is_empty() && requested_rules.contains(rule))
+                || (reverse_deps
+                    && deps.rules.iter().any(|d| nodes.contains(d)))
+            {
+                generate_node_for_ident(
+                    &mut graph,
+                    &rule,
+                    &deps,
+                    &mut nodes,
+                    &dep_map,
+                    reverse_deps,
+                );
+            }
+        }
+    }
+
+    // Now that writer is out of scope we can read from bytes again.
+    String::from_utf8(bytes).unwrap()
+}
+
+fn generate_node_for_ident<'a>(
+    graph: &mut Scope,
+    ident: &'a str,
+    deps: &Deps<'a>,
+    nodes: &mut HashSet<&'a str>,
+    dep_map: &BTreeMap<&str, Deps<'a>>,
+    reverse_deps: bool,
+) {
+    {
+        let mut node = graph.node_named(ident);
+        node.set_fill_color(Color::PaleTurquoise).set_style(Style::Filled);
+        nodes.insert(ident);
+    }
+
+    if reverse_deps {
+        for rule_dep in deps.rules.iter() {
+            if nodes.contains(rule_dep) {
+                graph.edge(ident, rule_dep);
+            }
+        }
+        return;
+    }
+
+    for rule_dep in deps.rules.iter() {
+        if !nodes.contains(*rule_dep) {
+            generate_node_for_ident(
+                graph,
+                rule_dep,
+                &dep_map[rule_dep],
+                nodes,
+                dep_map,
+                reverse_deps,
+            );
+        }
+        graph.edge(ident, rule_dep);
+    }
+
+    for module in deps.modules.iter() {
+        if !nodes.contains(*module) {
+            let mut node = graph.node_named(*module);
+            node.set_fill_color(Color::PaleGreen).set_style(Style::Filled);
+            nodes.insert(module);
+        }
+        graph.edge(ident, module);
+    }
+
+    for unknown in deps.unknowns.iter() {
+        if !nodes.contains(*unknown) {
+            let mut node = graph.node_named(*unknown);
+            node.set_fill_color(Color::Red).set_style(Style::Filled);
+            nodes.insert(unknown);
+        }
+        graph.edge(ident, unknown);
+    }
+}
+
+fn check_expr<'a>(
+    expr: &'a Expr<'a>,
+    rule_name: &'a str,
+    dep_map: &mut BTreeMap<&'a str, Deps<'a>>,
+) {
+    let mut variables: Vec<&str> = vec![];
+    for event in DFSIter::new(expr) {
+        match event {
+            DFSEvent::Enter(expr) => match expr {
+                Expr::Ident(ident) => {
+                    if let Some(_) = dep_map.get(ident.name) {
+                        // This is an identifier that matches a previously seen
+                        // rule.
+                        dep_map.entry(rule_name).and_modify(|v| {
+                            v.rules.insert(ident.name);
+                        });
+                    } else if yara_x::mods::module_names()
+                        .any(|module| module == ident.name)
+                        && !variables.contains(&ident.name)
+                    {
+                        // This is a known module or is not in the list of
+                        // variable identifier to be ignored.
+                        dep_map.entry(rule_name).and_modify(|v| {
+                            v.modules.insert(ident.name);
+                        });
+                    } else {
+                        dep_map.entry(rule_name).and_modify(|v| {
+                            v.unknowns.insert(ident.name);
+                        });
+                    }
+                }
+                Expr::Of(of) => match &of.quantifier {
+                    Quantifier::Percentage(quantifier)
+                    | Quantifier::Expr(quantifier) => {
+                        check_expr(&quantifier, rule_name, dep_map);
+                    }
+                    _ => {}
+                },
+                Expr::ForOf(for_of) => match &for_of.quantifier {
+                    Quantifier::Percentage(quantifier)
+                    | Quantifier::Expr(quantifier) => {
+                        check_expr(&quantifier, rule_name, dep_map);
+                    }
+                    _ => {}
+                },
+                Expr::ForIn(for_in) => {
+                    variables =
+                        for_in.variables.iter().map(|v| v.name).collect();
+                    match &for_in.quantifier {
+                        Quantifier::Percentage(quantifier)
+                        | Quantifier::Expr(quantifier) => {
+                            check_expr(&quantifier, rule_name, dep_map);
+                        }
+                        _ => {}
+                    }
+                }
+                Expr::With(with) => {
+                    variables = with
+                        .declarations
+                        .iter()
+                        .map(|d| d.identifier.name)
+                        .collect();
+                }
+                _ => {}
+            },
+            DFSEvent::Leave(expr) => match expr {
+                Expr::ForIn(_) | Expr::With(_) => {
+                    variables.clear();
+                }
+                _ => {}
+            },
+        }
+    }
 }
