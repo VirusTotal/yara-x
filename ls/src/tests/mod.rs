86d0462459cc0a8c1c1722d14291646908b6da3d
prosperritty <albert.tikaiev@gmail.com>
2026-02-12 14:40:29 +0100

    test(ls): add tests related to identifiers from `with` and `for` statement

--- ls/src/tests/mod.rs
+++ ls/src/tests/mod.rs
@@ -169,6 +169,9 @@ async fn rename() {
     test_lsp_request::<_, Rename>("rename3.yar").await;
     test_lsp_request::<_, Rename>("rename4.yar").await;
     test_lsp_request::<_, Rename>("rename5.yar").await;
+    test_lsp_request::<_, Rename>("rename6.yar").await;
+    test_lsp_request::<_, Rename>("rename7.yar").await;
+    test_lsp_request::<_, Rename>("rename8.yar").await;
 }
 
 #[tokio::test]
@@ -178,6 +181,8 @@ async fn references() {
     test_lsp_request::<_, References>("references3.yar").await;
     test_lsp_request::<_, References>("references4.yar").await;
     test_lsp_request::<_, References>("references5.yar").await;
+    test_lsp_request::<_, References>("references6.yar").await;
+    test_lsp_request::<_, References>("references7.yar").await;
 }
 
 #[tokio::test]
@@ -187,6 +192,8 @@ async fn goto_definition() {
     test_lsp_request::<_, GotoDefinition>("goto3.yar").await;
     test_lsp_request::<_, GotoDefinition>("goto4.yar").await;
     test_lsp_request::<_, GotoDefinition>("goto5.yar").await;
+    test_lsp_request::<_, GotoDefinition>("goto6.yar").await;
+    test_lsp_request::<_, GotoDefinition>("goto7.yar").await;
 }
 
 #[tokio::test]
@@ -198,6 +205,8 @@ async fn hover() {
     test_lsp_request::<_, HoverRequest>("hover5.yar").await;
     test_lsp_request::<_, HoverRequest>("hover6.yar").await;
     test_lsp_request::<_, HoverRequest>("hover7.yar").await;
+    test_lsp_request::<_, HoverRequest>("hover8.yar").await;
+    test_lsp_request::<_, HoverRequest>("hover9.yar").await;
 }
 
 #[tokio::test]
@@ -218,6 +227,9 @@ async fn document_highlights() {
     test_lsp_request::<_, DocumentHighlightRequest>("highlights3.yar").await;
     test_lsp_request::<_, DocumentHighlightRequest>("highlights4.yar").await;
     test_lsp_request::<_, DocumentHighlightRequest>("highlights5.yar").await;
+    test_lsp_request::<_, DocumentHighlightRequest>("highlights6.yar").await;
+    test_lsp_request::<_, DocumentHighlightRequest>("highlights7.yar").await;
+    test_lsp_request::<_, DocumentHighlightRequest>("highlights8.yar").await;
 }
 
 #[tokio::test]
@@ -273,6 +285,8 @@ async fn completion() {
 
     #[cfg(all(feature = "full-compiler", not(feature = "magic-module")))]
     test_lsp_request::<_, Completion>("completion13.yar").await;
+
+    test_lsp_request::<_, Completion>("completion14.yar").await;
 }
 
 #[tokio::test]
---
+++ ls/src/tests/testdata/references6.yar
@@ -0,0 +1,10 @@
+rule ident {
+  condition:
+    true
+}
+
+rule with_the_same_ident {
+  condition:
+    ident and with
+      ident = 10: (ident == 5 + 5)
+}
