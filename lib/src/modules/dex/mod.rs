efb5cada8b971be9d6405bcf73cf107bbd930368
Victor M. Alvarez <vmalvarez@virustotal.com>
2026-05-15 12:27:52 +0200

    fix: issue in the `contains_method` function in the `dex` module.
    This function assumed that the `methods` vector is sorted by name and uses a binary search, but that's not the case.

--- lib/src/modules/dex/mod.rs
+++ lib/src/modules/dex/mod.rs
@@ -25,6 +25,7 @@ thread_local!(
 fn main(data: &[u8], _meta: Option<&[u8]>) -> Result<Dex, ModuleError> {
     CHECKSUM_CACHE.with(|cache| *cache.borrow_mut() = None);
     SIGNATURE_CACHE.with(|cache| *cache.borrow_mut() = None);
+
     match parser::Dex::parse(data) {
         Ok(dex) => Ok(dex.into()),
         Err(_) => {
@@ -137,7 +138,7 @@ fn contains_method(
         Err(_) => return None,
     };
 
-    Some(dex.methods.binary_search_by(|item| item.name.cmp(&str)).is_ok())
+    Some(dex.methods.iter().any(|item| item.name.as_deref() == str.as_deref()))
 }
 
 /// Function that checks whether the DEX file contains the specified class
