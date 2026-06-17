75d63945b7f7f812737b55311327dca88df5f8fa
Victor M. Alvarez <vmalvarez@virustotal.com>
2026-06-17 16:02:02 +0200

    style: fix clippy warning.

--- lib/src/modules/mod.rs
+++ lib/src/modules/mod.rs
@@ -492,7 +492,7 @@ pub mod mods {
 
             /// Returns the documentation for the current field.
             pub fn doc(&self) -> Option<&str> {
-                self.struct_field.doc.as_deref()
+                self.struct_field.doc
             }
         }
 
